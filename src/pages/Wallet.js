46117de1b4b631632eb6151062417df310c57a75
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-10-07 16:44:13 -0300

    Requisito 5 completo

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <p>Alingria</p>
+      <Header />
     );
   }
 }
