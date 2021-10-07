39667e0861531f7e405a859cd3befb35a9d895ec
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-10-07 16:22:05 -0300

    Requisito 1 completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <section>
+          <h1>Login</h1>
+          <form>
+            <label htmlFor="email">
+              Email:
+              <input
+                data-testid="email-input"
+                type="email"
+                name="email"
+                id="email"
+              />
+            </label>
+            <label htmlFor="password">
+              Password:
+              <input
+                data-testid="password-input"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </label>
+            <button
+              type="button"
+            >
+              Entrar
+            </button>
+          </form>
+        </section>
+      </main>
+    );
   }
 }
 
