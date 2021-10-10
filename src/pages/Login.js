94f6e84cc44a7a6f74d6a4f979d7359bfeff1364
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-10-10 12:38:38 -0300

    Initial commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div className="page">
+        <div className="login">
+          <h1>Trybewallet</h1>
+
+          <input
+            type="text"
+            value={ email }
+            onChange={ this.handleChange }
+            placeholder="Email"
+            className="form-control"
+          />
+
+        </div>
+      </div>
+    );
   }
 }
 
