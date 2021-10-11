e81d8ed4050f9ef1b21a087b17102920e0d41c28
Pablo Assunção <pabloassuncao2@gmail.com>
2021-10-11 00:34:06 -0300

    Adiciona os requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import NotFound from './pages/NotFound';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/carteira" component={ Wallet } />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  onClickBtn() {
+    const { history } = this.props;
+    history.push
+  }
+
+  validateEmail() {
+    const { email } = this.state;
+    const re = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    return re.test(String(email).toLowerCase());
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const MIN_PASSWORD_LENGTH = 6;
+    return (
+      <>
+        <input
+          data-testid="email-input"
+          onChange={ this.handleChange }
+          type="text"
+          name="email"
+          value={ email }
+        />
+        <input
+          data-testid="password-input"
+          onChange={ this.handleChange }
+          type="password"
+          name="password"
+          value={ password }
+        />
+        <button
+          type="button"
+          disabled={ !this.validateEmail()
+            || password.length < MIN_PASSWORD_LENGTH }
+          onClick={ this.onClickBtn }
+        >
+          Entrar
+        </button>
+      </>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default Login;
