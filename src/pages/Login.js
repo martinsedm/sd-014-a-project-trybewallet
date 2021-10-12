3a965af884b821f9cfa0af4812ea8cb3775598ea
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-11 21:03:52 -0300

    Add requirement 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isEnable: false,
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { isEnable } = this.state;
+    return (
+      <form>
+        <input type="email" data-testid="email-input" placeholder="Email" />
+        <input type="text" data-testid="password-input" placeholder="Email" />
+        <input type="submmit" value="Entrar" disabled={ !isEnable } />
+      </form>
+    );
   }
 }
 
