7a8d2f2d7f2d1395b40e57fdd2d1a46513db3e36
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-10-12 17:20:38 -0300

    Requisito 10

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 import { connect } from 'react-redux';
 
-export default class Expense extends Component {
+import { removeExpense as remove } from '../actions';
+
+class Expense extends Component {
   render() {
-    const { expense } = this.props;
-    const { description, tag, method, currency, value, exchangeRates } = expense;
+    const { expense, removeExpense } = this.props;
+    const { description, tag, method, currency, value, exchangeRates, id } = expense;
     const { name, ask } = exchangeRates[currency];
 
     return (
@@ -13,34 +16,38 @@ export default class Expense extends Component {
         <div className="col" role="cell">{ description}</div>
         <div className="col" role="cell">{tag}</div>
         <div className="col" role="cell">{method}</div>
-        <div
-          className="col"
-          role="cell"
-        >
-          {`${value}`}
-
-        </div>
+        <div className="col" role="cell">{`${value}`}</div>
         <div className="col" role="cell">{name.split('/')[0]}</div>
-        <div
-          className="col"
-          role="cell"
-        >
-          {`${parseFloat(ask).toFixed(2)}`}
-
-        </div>
-        <div
-          className="col"
-          role="cell"
-        >
+        <div className="col" role="cell">{`${parseFloat(ask).toFixed(2)}`}</div>
+        <div className="col" role="cell">
           {`${(parseFloat(value) * parseFloat(ask)).toFixed(2)}`}
-
         </div>
         <div className="col" role="cell">Real</div>
-        <div className="col" role="cell">botão</div>
+        <div className="col" role="cell">
+          <button
+            type="button"
+            className="btn btn-warning expense-btn"
+          >
+            <AiOutlineEdit />
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger expense-btn "
+            data-testid="delete-btn"
+            onClick={ () => removeExpense(id) }
+          >
+            <AiOutlineDelete />
+          </button>
+
+        </div>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  removeExpense: (id) => dispatch(remove(id)),
+});
 
 Expense.propTypes = {
   expense: PropTypes.shape({
@@ -52,4 +59,7 @@ Expense.propTypes = {
     tag: PropTypes.string.isRequired,
     exchangeRates: PropTypes.objectOf(PropTypes.object).isRequired,
   }).isRequired,
+  removeExpense: PropTypes.func.isRequired,
 };
+
+export default connect(null, mapDispatchToProps)(Expense);
