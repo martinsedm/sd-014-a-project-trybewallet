27f1d249bcccc70b1044a089d599b988c2fe235b
Pally <francisca.bezerr@gmail.com>
2021-10-13 20:24:27 -0300

    questao 2 ok

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // Função genérica para salvar o que é digitado no input dentro do estado
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { email, password } = this.state;
+    const MAXLENGTH = 6;
+    const validateEmail = /\S+@\S+\.\S+/.test(email);
+    // https://backefront.com.br/validacao-email-javascript/
     return (
       <fieldset>
         <form>
@@ -11,18 +31,22 @@ class Login extends React.Component {
               data-testid="email-input"
               type="email"
               name="email"
+              value={ email }
+              onChange={ this.handleChange }
             />
           </label>
-          <label htmlFor="senha">
+          <label htmlFor="password">
             Password:
             <input
               data-testid="password-input"
               type="password"
-              name="senha"
+              name="password"
+              value={ password }
+              onChange={ this.handleChange }
             />
           </label>
         </form>
-        <button type="submit" disabled>Entrar</button>
+        <button type="submit" disabled={ !validateEmail || password.length < MAXLENGTH }>Entrar</button>
       </fieldset>
 
     );
