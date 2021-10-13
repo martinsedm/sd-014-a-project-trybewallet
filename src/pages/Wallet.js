56bf69a1db177190e3edb82b69aaa968357795cf
Pablo Assunção <pabloassuncao2@gmail.com>
2021-10-13 18:16:31 -0300

    Adiciona o requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import '../css/Wallet.css';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, total } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">{total || 0}</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+function mapStateToProps(state) {
+  return {
+    email: state.user.email,
+    total: state.wallet.total,
+  };
+}
+
+export default connect(mapStateToProps)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,19 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  total: '',
+};
+
+export default function user(
+  state = initialState, { payload, type },
+) {
+  switch (type) {
+  case 'TOTAL':
+    return ({
+      ...state,
+      email: payload,
+    });
+
+  default:
+    return (state);
+  }
+}
