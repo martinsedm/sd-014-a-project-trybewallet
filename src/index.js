dccae5e0ec2e12a9fa4ae0c68c6e581f6a769f43
rodPinheiro <rod_bma@hotmail.com>
2021-10-13 22:11:45 -0300

    Requisito 3

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import * as serviceWorker from './serviceWorker';
 import store from './store/index';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={ store }>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { actionRegisterUser } from '../actions';
 
 class Login extends React.Component {
   constructor() {
@@ -8,44 +10,56 @@ class Login extends React.Component {
       login: '',
       password: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange = ({ target }) => {
+  handleChange({ target }) {
     const { name, value } = target;
     this.setState({
       [name]: value,
     });
-  };
+  }
 
-  handleClick = () => {
-   
+  handleClick() {
+    const { login } = this.state;
+    const { history, registerUser } = this.props;
+    console.log(history);
+    registerUser(login);
+    history.push('/carteira');
   }
 
   render() {
     const { login, password } = this.state;
     const regex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+    const MAX_LENGTH = 6;
     return (
       <div>
         <form>
-          <label htmlFor='login'>
+          <label htmlFor="login">
             <input
-              data-testid='email-input'
-              onChange={this.handleChange}
-              name='login'
-              id='login'
-              type='email'
+              data-testid="email-input"
+              onChange={ this.handleChange }
+              name="login"
+              id="login"
+              type="email"
             />
           </label>
-          <label htmlFor='password'>
+          <label htmlFor="password">
             <input
-              data-testid='password-input'
-              onChange={this.handleChange}
-              name='password'
-              id='password'
-              type='password'
+              data-testid="password-input"
+              onChange={ this.handleChange }
+              name="password"
+              id="password"
+              type="password"
             />
           </label>
-          <button type='button' onClick={this.handleChange} data-testid='my-action' disabled={ !regex.test(login) || password.length < 6 } >
+          <button
+            type="button"
+            onClick={ this.handleClick }
+            data-testid="my-action"
+            disabled={ !regex.test(login) || password.length < MAX_LENGTH }
+          >
             Entrar
           </button>
         </form>
@@ -54,4 +68,16 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  registerUser: (payload) => dispatch(actionRegisterUser(payload)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    action: PropTypes.string.isRequired,
+    push: PropTypes.func,
+  }).isRequired,
+  registerUser: PropTypes.func.isRequired,
+};
