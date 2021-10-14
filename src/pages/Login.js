aa9e749a953be30b2c207fe7ca6419cc6fddb427
BrianCaetano <brian-caetano@hotmail.com>
2021-10-14 15:43:15 -0300

    Login.js (Req 1, 2 e 3)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux';
+import { saveEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+    };
+  };
+  
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleClick = () => {
+    const { email} = this.state;
+    const { registerUser } = this.props;
+    this.setState({ redirect: true})
+    registerUser(email);
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, redirect } = this.state;
+    const MIN_CHARACTER = 6;
+    // dica de regex com Pedro Alles T13 & https://regex101.com/library/jZ2cC4
+    const REGEX_VALID_EMAIL = /^([a-z0-9]+(?:[._-][a-z0-9]+)*)@([a-z0-9]+(?:[.-][a-z0-9]+)*\.[a-z]{2,})$/i;
+    
+    if (redirect) return <Redirect to="/carteira" />
+
+    return (
+      <main>
+        <label htmlFor="email">
+          E-mail
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={ password.length < MIN_CHARACTER || !REGEX_VALID_EMAIL.test(email) }
+          onClick={ this.handleClick}
+        >
+          Entrar
+        </button>
+      </main>
+    );
   }
 }
 
-export default Login;
+/* const mapStateToProps = (state) => ({
+  loginEmail: state.user.email,
+}) */
+
+const mapDispatchToProps = (dispatch) => (
+  {
+    registerUser: (payload) => dispatch(saveEmail(payload)),
+  }
+);
+
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SAVE_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case SAVE_EMAIL:
+    return {
+      ...state, email: payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
