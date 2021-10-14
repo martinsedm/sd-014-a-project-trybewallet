bebaada4177b347a05e47d0a409c0b48119825a8
tiagosathler <sathler@gmail.com>
2021-10-14 20:50:37 -0300

    Requisitos 4 e 5 - Wallet: concluídos; implementa Wallet, conecta ao Redux e implementa componente Header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { email } = props.user;
+    this.state = {
+      email,
+      total: 0,
+      currency: 'BRL',
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, total, currency } = this.state;
+    return (
+      <Header data={ { email, total, currency } } />
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  user: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
