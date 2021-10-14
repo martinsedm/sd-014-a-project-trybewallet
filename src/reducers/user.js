33302276e24611e69f885cab028759c419eb79c2
BlyNTC <isaacase@hotmail.com>
2021-10-13 23:43:48 -0300

    requisito 1

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,24 @@
+import { SAVE_EMAIL } from '../actions';
+
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+const userReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case SAVE_EMAIL:
+    return {
+      ...state, email: payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
