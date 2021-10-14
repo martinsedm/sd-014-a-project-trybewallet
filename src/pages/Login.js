58f6d88801caa3d15a049fafed9bd061b8604dea
rodPinheiro <rod_bma@hotmail.com>
2021-10-13 21:25:03 -0300

    Requisitos 1 e 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import {Link, Redirect} from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      login: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = () => {
+   
+  }
+
   render() {
-    return <div>Login</div>;
+    const { login, password } = this.state;
+    const regex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+    return (
+      <div>
+        <form>
+          <label htmlFor='login'>
+            <input
+              data-testid='email-input'
+              onChange={this.handleChange}
+              name='login'
+              id='login'
+              type='email'
+            />
+          </label>
+          <label htmlFor='password'>
+            <input
+              data-testid='password-input'
+              onChange={this.handleChange}
+              name='password'
+              id='password'
+              type='password'
+            />
+          </label>
+          <button type='button' onClick={this.handleChange} data-testid='my-action' disabled={ !regex.test(login) || password.length < 6 } >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
