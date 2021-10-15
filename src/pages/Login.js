defdf85c610f26d7420f6734f0a4eedca5d89820
AlineCarolina <eualine2017@gmail.com>
2021-10-15 18:10:47 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      senha: '',
+    };
+    this.handleChance = this.handleChance.bind(this);
+  }
+
+  handleChance({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, senha } = this.state;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          onChange={ this.handleChance }
+          value={ email }
+          name="email"
+        />
+        <input
+          data-testid="password-input"
+          onChange={ this.handleChance }
+          value={ senha }
+          name="senha"
+          type="password"
+        />
+        <button type="submit">
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
