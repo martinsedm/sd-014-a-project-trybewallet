b83090cd589aed27a0a9b5852de486b3700a23a9
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-10-15 18:25:53 -0300

    Requisito 1 e requisito 2(incompleto)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,55 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <fieldset>
+        <label htmlFor="login">
+          <input
+            id="login"
+            data-testid="email-input"
+            type="email"
+            onChange={ this.handleChange }
+            value={ email }
+            name="email"
+            placeholder="Digite seu email"
+            required
+          />
+          <input
+            id="login"
+            data-testid="password-input"
+            type="text"
+            onChange={ this.handleChange }
+            value={ password }
+            name="password"
+            placeholder="Digite sua senha"
+            required
+          />
+          <button
+            type="button"
+            disabled
+          >
+            Entrar
+          </button>
+        </label>
+      </fieldset>
+    );
   }
 }
 
