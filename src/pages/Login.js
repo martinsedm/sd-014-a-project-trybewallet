59df9a8b1417283aa805affe113a28c8cb504129
Gabriel <gabrielferreiradsl@hotmail.com>
2021-10-15 15:29:54 -0300

    Requisito 1 completo, e alteraçoes no reducer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <h3>Login</h3>
+        <input type="text" name="login" id="login" data-testid="email-input" />
+        <input type="text" name="password" id="password" data-testid="password-input" />
+        <div>
+          <button type="button">Entrar</button>
+        </div>
+      </main>
+    );
   }
 }
 
