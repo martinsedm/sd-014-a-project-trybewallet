c7c85eba64875c891681d6b98b40d49521556d35
Guinxard <lucasmfarias3@gmail.com>
2021-10-15 03:25:26 -0300

    requisito 1 ok

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,9 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const USER_INITIALSTATE = {
+  user: {
+    email: '',
+  },
+};
+
+const reducer = (state = USER_INITIALSTATE) => state;
+
+export default reducer;
