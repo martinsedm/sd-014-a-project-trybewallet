2fed3d5853c2a42fd112512f1d658cc42c4711cd
jagrunewald <je_grunewald@hotmail.com>
2021-10-15 20:33:14 -0300

    Requisito 1 - com ajustes lint

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,6 +1,6 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
-const initialState = {
-  user: {
-    email: '',
-  },
-};
+// const initialState = {
+//   user: {
+//     email: '',
+//   },
+// };
