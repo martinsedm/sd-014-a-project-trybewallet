6de17bc602ac5d19bdbaaeb122454822d03b3002
Douglas Souza <douglassouzabjj@hotmail.com>
2021-10-15 16:56:49 -0300

    c4 - Requisito 4

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <div>
-      <div>Hello, TrybeWallet!</div>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/carteira" component={ Wallet } />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
+    </Switch>
   );
 }
 
