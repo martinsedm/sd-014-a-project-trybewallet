6bbd84fdf309cef5313a34b33e671056084f32e1
Augusto <augusto.raminelli>
2021-10-15 17:53:42 -0300

    requisito 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const SET_EMAIL_USER = 'SET_EMAIL_USER';
+
+export const setEmailUser = (email) => ({
+  type: SET_EMAIL_USER,
+  payload: email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import { setEmailUser } from '../actions';
 import logo from '../logo.svg';
 
 import '../style/login.css';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      MIN_LENGTH_PASS: 6,
+    };
+  }
+
+  // validando os campos do input Login fonte https://pt.stackoverflow.com/questions/283129/formulario-email-react
+  emailValid() {
+    const { email } = this.state;
+    if (email) {
+      const validEmail = /\S+@\S+\.\S+/;
+      return validEmail.test(email);
+    }
+  }
+
+  passwordValid() {
+    const { password, MIN_LENGTH_PASS } = this.state;
+    if (password.length >= MIN_LENGTH_PASS) return true;
+    return false;
+  }
+
   render() {
+    const { email, password } = this.state;
+    const { getEmail } = this.props;
     return (
       <div className="ctn-login">
         <img className="login-logo" alt="Trybe Wallet Logo" src={ logo } />
@@ -12,28 +43,40 @@ class Login extends React.Component {
         <input
           className="login-input"
           data-testid="email-input"
-          name="email"
+          value={ email }
           type="email"
           placeholder="Informe seu email"
-          // onChange={ this.handleChange }
+          onChange={ (e) => this.setState({ email: e.target.value }) }
         />
         <input
           className="login-input"
           data-testid="password-input"
           type="password"
-          name="password"
+          value={ password }
           placeholder="Informe sua senha"
-          // onChange={ this.handleChange }
+          onChange={ (e) => this.setState({ password: e.target.value }) }
         />
-        <button
-          className="login-button"
-          type="button"
-        >
-          Entrar
-        </button>
+        <Link to="/carteira">
+          <button
+            className="login-button"
+            type="button"
+            disabled={ !this.emailValid() || !this.passwordValid() }
+            // onClick={ () => getEmail(email) }
+          >
+            Entrar
+          </button>
+        </Link>
       </div>
     );
   }
 }
 
-export default Login;
+// const mapDispatchToProps = (dispatch) => ({
+//   getEmail: (email) => dispatch(setEmailUser(email)),
+// });
+
+Login.propTypes = {
+  getEmail: PropTypes.func.isRequired,
+};
+
+export default(Login);
