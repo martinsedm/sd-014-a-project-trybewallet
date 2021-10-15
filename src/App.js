f2e3c7b8c143e6e17759f88c017f5b2686ab93bf
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-10-15 15:02:30 -0300

    implementing reducers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
-import Login from './pages/Login';
-import Wallet from './pages/Wallet';
 
 function App() {
-  return (
-    <Switch>
-      <Route exact path="/" render={ (props) => <Login { ...props } /> } />
-      <Route exact path="/carteira" render={ (props) => <Wallet { ...props } /> } />
-    </Switch>
-  );
+  return <div>Hello, TrybeWallet!</div>;
 }
 
 export default App;
