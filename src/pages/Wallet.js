4ebbba1201066537459d2d0af9ae1d2ab0aeb789
MuriloM <muriloommaia@gmail.com>
2021-10-15 15:59:13 -0300

    requisito 4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Wallet extends React.Component {
+export default class Wallet extends Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        carteira
+      </div>
+    );
   }
 }
-
-export default Wallet;
