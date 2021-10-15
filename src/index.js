a521c491c84a75977c15dda607b4946b40f44edc
Jonathan Ferreira <j0n4t@hotmail.com>
2021-10-15 18:27:24 -0300

    Requisito 01 - Página inicial (login)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+
 import * as serviceWorker from './serviceWorker';
+import store from './store';
+
+import App from './App';
+
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <h1>Trybe Wallet</h1>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Senha"
+          />
+          <input
+            type="submit"
+            value="Entrar"
+          />
+        </form>
+      </section>
+    );
   }
 }
 
