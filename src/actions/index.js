0ca4a620e50cadfc9d4721eadd8c2b2318a8f8ba
Matheus Souza <ms.conrado25@hotmail.com>
2021-10-15 16:00:39 -0300

    requisito 1 - craindo a pagina inicial de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-// Coloque aqui suas actions
+export const USER = 'USER';
+export const WALLET = 'WALLET';
+
+export const userAction = (email) => ({
+  type: USER,
+  payload: {
+    email,
+  },
+});
+
+export const walletAction = (currencies, expenses) => ({
+  type: WALLET,
+  payload: {
+    currencies,
+    expenses,
+  },
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { userAction } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value } = target;
+    this.setState({
+      email: value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email } = this.state;
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={ email }
+          onChange={ this.handleChange }
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+        />
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  user: (email) => dispatch(userAction(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
