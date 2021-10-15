a954cf37ef799d7eb768f022e6452c071a8959af
Écio Ferraz <ecioferraz@hotmail.com>
2021-10-15 12:37:28 -0300

    req 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,11 @@ class Header extends Component {
   render() {
     const { email } = this.props;
     return (
-      <div>
+      <header>
         <p data-testid="email-field">{ email }</p>
-      </div>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
