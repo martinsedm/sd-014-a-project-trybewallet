c9809d58ee8d4aad800c4535475b891b782462cf
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-10-15 15:32:25 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,41 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <fieldset>
+          <legend>Faça seu login</legend>
+          <label htmlFor="email">
+            E-mail
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Digite seu e-mail"
+              data-testid="email-input"
+              // onChange={ }
+            />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Digite sua senha"
+              data-testid="password-input"
+              // onChange={ }
+            />
+          </label>
+        </fieldset>
+        <button
+          type="button"
+          // onClick={ }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
