f6dda4ca99abc66cc95ea36a729076f0fdeb037d
Welton Lopes <weltonlopesmetal@gmail.com>
2021-10-15 11:34:06 -0300

    Testando a funçao 1, muito inicial

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <div>Login</div>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              id="email"
+              type="email"
+              name="email"
+              value="emailinput"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value="passwordinput"
+              data-testid="password-input"
+            />
+          </label>
+        </form>
+      </>
+    );
   }
 }
 
