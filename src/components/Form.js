e5c4ba91f9cca0d34efb6535a0bdcd5ab4e02927
MuriloM <muriloommaia@gmail.com>
2021-10-15 17:04:08 -0300

    requisito 7

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,8 +12,23 @@ export default class Form extends Component {
       coin: '',
       payment: 'Dinheiro',
       tag: 'Alimentação',
+      coinOption: [],
     };
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCoinAPI();
+  }
+
+  async fetchCoinAPI() {
+    const coinFetch = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const coinJSON = await coinFetch.json();
+    const coinKeys = Object.keys(coinJSON);
+    const coinOption = coinKeys.filter((coin) => coin !== 'USDT');
+    this.setState({
+      coinOption,
+    });
   }
 
   handleChange({ target }) {
@@ -24,7 +39,7 @@ export default class Form extends Component {
   }
 
   render() {
-    const { value, description, coin, payment, tag } = this.state;
+    const { value, description, coin, payment, tag, coinOption } = this.state;
     const optionsCard = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
     const optionTag = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
     return (
@@ -45,7 +60,12 @@ export default class Form extends Component {
         >
           Descrição
         </Input>
-        <Select name="coin" option={ [] } onChange={ this.handleChange } value={ coin }>
+        <Select
+          name="coin"
+          option={ coinOption }
+          onChange={ this.handleChange }
+          value={ coin }
+        >
           Moeda
         </Select>
         <Select
