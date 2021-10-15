733df5b0fc63b966ec9db2dc250598b284abf9bd
Samuel Nascimento <sfsnascimento@gmail.com>
2021-10-15 17:32:07 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,68 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.emailValidation = this.emailAndPassowordValidation.bind(this);
+  }
+
+  emailAndPassowordValidation() {
+    const PASSWORD_LENGTH = 5;
+    const { email, password } = this.state;
+    // Regex utilizada foi retirada de https://stackoverflow.com/questions/53377994/email-validation-in-reactjs-is-not-working-properly
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    if (emailRegex.test(email) && password.length >= PASSWORD_LENGTH) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.emailAndPassowordValidation();
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div>
         <form>
           <div>
-            Email:
-            <input type="email" data-testid="email-input" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+            />
           </div>
           <br />
           <div>
-            Senha:
-            <input type="password" data-testid="password-input" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+            />
           </div>
           <br />
-          <button type="submit">Entrar</button>
+          <button
+            type="submit"
+            disabled={ disabled }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
