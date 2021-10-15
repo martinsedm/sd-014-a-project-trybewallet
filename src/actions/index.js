549c49db8bb141e517df86995572edfba10461be
thiagoalmeida22 <thiagoguimaraesalmeida@gmail.com>
2021-10-15 15:54:01 -0300

    Iniciando o projeto

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const PERSONAL_HANDLE = 'PERSONAL_HANDLE';
+export const PROFESSIONAL_HANDLE = 'PROFESSIONAL_HANDLE';
+
+export const personalHandle = (personalInfo) => ({
+  type: PERSONAL_HANDLE,
+  payload: personalInfo,
+});
+
+export const professionalHandle = (professionalInfo) => ({
+  type: PROFESSIONAL_HANDLE,
+  payload: professionalInfo,
+});
