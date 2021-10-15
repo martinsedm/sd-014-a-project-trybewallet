cbc6aa41126958bc705277fdbc87fe540ff89537
WalmirLucena <walmirlucena3@gmail.com>
2021-10-15 16:30:51 -0400

    Requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 
-class App extends React.Component() {
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+
+class App extends React.Component {
   render() {
-    return <div>Hello, TrybeWallet!</div>;
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/carteira" component={ Wallet } />
+        </Switch>
+      </div>);
   }
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,80 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDisable = this.handleDisable.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  // referência https://stackoverflow.com/questions/43676695/email-validation-react-native-returning-the-result-as-invalid-for-all-the-e
+  handleDisable() {
+    const { email, password } = this.state;
+    const MIN_LENGTH = 6;
+    const validEmail = (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w\w+)+$/).test(email);
+    if (validEmail && password.length >= MIN_LENGTH) {
+      return false;
+    }
+    return true;
+  }
+
+  handleSubmit() {
+    const { history } = this.props;
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <form onSubmit={ this.handleSubmit }>
+          <label htmlFor="email">
+            <input
+              type="text"
+              value={ email }
+              id="email"
+              name="email"
+              data-testid="email-input"
+              placeholder="Email"
+              onChange={ this.handleChange }
+              required
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="password"
+              value={ password }
+              id="password"
+              name="password"
+              data-testid="password-input"
+              placeholder="Senha"
+              onChange={ this.handleChange }
+              required
+            />
+          </label>
+          <button type="submit" disabled={ this.handleDisable() }>Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
