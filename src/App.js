0ecde63a561c9e26ed573aa58e3d566bf18e68fd
Maurício Leite <mauricioleite1@gmail.com>
2021-10-15 20:38:10 -0300

    Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={ Login } />
+  </Switch>
+);
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+const Login = () => (
+  <div>
+    <input type="email" data-testid="email-input" />
+    <input type="password" data-testid="password-input" />
+    <button type="submit">Entrar</button>
+  </div>
+);
 
 export default Login;
