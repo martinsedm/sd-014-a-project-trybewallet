785e826fe20b10be73f42edb4e6239a54bc32c6f
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-10-15 16:58:46 -0300

    C1 - Adicionado requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email">
+          <input
+            type="text"
+            id="email"
+            data-testid="email-input"
+            placeholder="E-mail"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            id="password"
+            data-testid="password-input"
+            placeholder="password"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
