daca58ee820900e5c1aa0d40c9e4ba74164a1874
pedrobuente <pedrobuente@gmail.com>
2021-10-15 18:06:06 -0300

    requisito 1 e 2 correcao styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 export class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+
       </Switch>
     );
   }
