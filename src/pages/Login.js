2dca02f696cc318d0d98afcc9040cf7af1dbe167
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-10-15 15:02:48 -0300

    inicio do preparo dos arquivos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,44 @@
 import React from 'react';
+// import { connect } from 'react-redux';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      disabledButton: true,
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disabledButton } = this.state;
+    return (
+      <div>
+        <div>
+          <input
+            type="email"
+            placeholder="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            type="password"
+            placeholder="senha"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </div>
+        <button
+          type="button"
+          disabled={ disabledButton }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
