db447000b8345e542e164dfa3c0e139f39ffc712
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-10-15 16:50:36 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      validData: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.enableButton = this.enableButton.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    },
+    () => {
+      this.enableButton();
+    });
+  }
+
+  enableButton() {
+    const { email, password } = this.state;
+    const MINIMUM_CHARACTERS = 6;
+    const validEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w\w+)+$/.test(email);
+    const validPassword = password.length >= MINIMUM_CHARACTERS;
+    if (validEmail && validPassword) {
+      this.setState({
+        validData: true,
+      });
+    }
+  }
+  // Source: "https://stackoverflow.com/questions/43676695/email-validation-react-native-returning-the-result-as-invalid-for-all-the-e"
+
   render() {
+    const { email, password, validData } = this.state;
     return (
       <form>
         <fieldset>
@@ -12,9 +49,10 @@ class Login extends React.Component {
               type="text"
               id="email"
               name="email"
+              value={ email }
               placeholder="Digite seu e-mail"
               data-testid="email-input"
-              // onChange={ }
+              onChange={ this.handleChange }
             />
           </label>
           <label htmlFor="password">
@@ -23,17 +61,19 @@ class Login extends React.Component {
               type="password"
               id="password"
               name="password"
+              value={ password }
               placeholder="Digite sua senha"
               data-testid="password-input"
-              // onChange={ }
+              onChange={ this.handleChange }
             />
           </label>
         </fieldset>
         <button
           type="button"
+          disabled={ !validData }
           // onClick={ }
         >
-          Entrar
+          <Link to="/carteira">Entrar</Link>
         </button>
       </form>
     );
