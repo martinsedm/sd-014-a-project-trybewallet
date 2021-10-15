333417d34f6aba1dccbffebfb006b035d91b179e
Filipe <andradefilipe2002@gmail.com>
2021-10-15 19:08:29 -0300

    Resolve rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
-      <Route path="/carteira">
-        <Header />
-        <Wallet />
-      </Route>
+      <Route path="/carteira" component={ Wallet } />
       <Route path="/" component={ Login } />
     </Switch>
   );
