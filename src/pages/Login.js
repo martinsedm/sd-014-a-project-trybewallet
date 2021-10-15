cc04e4bdf73f8d459092923d31440db1e8324457
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-10-15 14:55:38 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,32 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <fieldset>
+        <legend>Login</legend>
+        <label htmlFor="email">
+          Email:
+          <input
+            data-testid="email-input"
+            id="email"
+            name="email"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            data-testid="password-input"
+            id="password"
+            name="password"
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </fieldset>
+    );
   }
 }
 
