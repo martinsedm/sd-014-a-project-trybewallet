504793ad408bbb815999eaf4bfb7815fdc2e11d4
Douglas Souza <douglassouzabjj@hotmail.com>
2021-10-15 15:12:59 -0300

    c1 - Requisito 1 - Adiciona campos de login e senha na pagina Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+          />
+          <button type="button"> Entrar </button>
+        </form>
+      </div>
+    );
   }
 }
 
