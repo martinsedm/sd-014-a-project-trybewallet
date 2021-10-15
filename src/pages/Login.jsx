e4c86e80188d019d31ca141339712225ad292797
Bernardo <benotargiacomo@gmail.com>
2021-10-15 16:07:02 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,54 +8,65 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
-
+      isValid: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({ [name]: value }, () => {
+      const { email, password } = this.state;
+      const MIN_LENGTH = 6;
+      const PASSWORD_LENGTH = password.length >= MIN_LENGTH;
+      const EMAIL_FORMAT = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i.test(email);
+      // const EMAIL_FORMAT = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi.test(email);
+
+      if (PASSWORD_LENGTH && EMAIL_FORMAT) {
+        this.setState({ isValid: true });
+      } else {
+        this.setState({ isValid: false });
+      }
     });
   }
 
   handleClick() {
+    const { history } = this.props;
     console.log('click');
+    history.push('/carteira');
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, isValid } = this.state;
     return (
       <div className="login-container">
         <main>
-          <label htmlFor="email">
-            Email
-            <input
-              id="email"
-              type="text"
-              name="email"
-              value={ email }
-              onChange={ this.handleChange }
-              data-testid="email-input"
-            />
-          </label>
-          <label htmlFor="password">
-            Senha
-            <input
-              id="password"
-              type="password"
-              name="password"
-              value={ password }
-              onChange={ this.handleChange }
-              data-testid="password-input"
-            />
-          </label>
+
+          <input
+            placeholder="Email"
+            type="text"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+
+          <input
+            placeholder="password"
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+
           <button
             type="submit"
             onClick={ this.handleClick }
+            disabled={ !isValid }
           >
             Entrar
           </button>
