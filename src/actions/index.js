e9e87a6ee7b010cd02ccd78bbddcee95272c8e5a
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-10-15 16:39:18 -0300

    requisito 1,2 ok

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
 // Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+// export const WALLET = 'WALLET';
+
+export const loginAction = (user) => ({
+  type: LOGIN,
+  payload: {
+    ...user,
+  },
+});
+
+// export const walletAction = () => ({
+//   type: WALLET,
+//   payload: {
+//     ...
+//   }
+// })
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,8 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loginAction } from '../actions';
 
 class Login extends React.Component {
   constructor(props) {
@@ -9,26 +12,47 @@ class Login extends React.Component {
       email: '',
       password: '',
       disabledButton: true,
+      logged: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // https://github.com/tryber/sd-014-a-project-trybewallet/blob/b555871f432b40d0521b656556c26b19c6395c70/src/pages/Login.js
+  // https://stackoverflow.com/questions/43676695/email-validation-react-native-returning-the-result-as-invalid-for-all-the-e
+  // Tirei como base o repositorio do Thiago e o stackoverflow para realizar a validação do email e senha.
+
   handleChange({ target: { name, value } }) {
     this.setState({
       [name]: value,
+    }, () => {
+      const { email, password } = this.state;
+      const MINIMO_PASS = 5;
+      const disabledButton = !(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w\w+)+$/.test(email)
+      && password.length > MINIMO_PASS);
+      this.setState({
+        disabledButton,
+      });
     });
   }
 
   handleClick() {
-
+    const { email } = this.state;
+    const { login } = this.props;
+    login({ email });
+    this.setState({
+      logged: true,
+    });
   }
 
   render() {
-    const { email, password, disabledButton } = this.state;
+    const { email, password, disabledButton, logged } = this.state;
+
+    if (logged) return (<Redirect to="/carteira" />);
+
     return (
-      <div>
+      <div name="loginForm" id="login-form">
         Faça seu login
         <div>
           <input
@@ -60,4 +84,12 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (payload) => dispatch(loginAction(payload)),
+});
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
