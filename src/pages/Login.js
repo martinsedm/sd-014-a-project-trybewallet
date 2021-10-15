15e879f50e8daae58660eb757df9b72795f1316e
Augusto <augusto.raminelli>
2021-10-15 16:02:18 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import logo from '../logo.svg';
+
+import '../style/login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="ctn-login">
+        <img className="login-logo" alt="Trybe Wallet Logo" src={ logo } />
+        <h1>TrybeWallet React</h1>
+        <input
+          className="login-input"
+          data-testid="email-input"
+          name="email"
+          type="email"
+          placeholder="Informe seu email"
+          // onChange={ this.handleChange }
+        />
+        <input
+          className="login-input"
+          data-testid="password-input"
+          type="password"
+          name="password"
+          placeholder="Informe sua senha"
+          // onChange={ this.handleChange }
+        />
+        <button
+          className="login-button"
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
