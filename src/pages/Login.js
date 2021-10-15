0778730d1d761743eb08b39d181a490881f1c6bd
brunofeu <brunofeu89@gmail.com>
2021-10-15 14:41:21 -0300

    add requirement 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="name">
+            Nome:
+            <input id="name" type="text" name="name" data-testid="email-input" />
+          </label>
+        </form>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input id="email" type="email" name="email" data-testid="password-input" />
+          </label>
+        </form>
+        <button type="button">Entrar</button>
+      </div>
+
+    );
   }
 }
 
