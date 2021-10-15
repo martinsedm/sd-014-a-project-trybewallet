22a8bc54fc83c523f2657e2cbee21e807cdf480a
Jonathan Ferreira <j0n4t@hotmail.com>
2021-10-15 20:25:43 -0300

    Requisito 02 - Campos de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,52 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  }
+
+  isValidEmail = (email) => email.match(/^[\w.]+@[\w.]+\w+\.\w+$/);
+
+  isValidLogin = () => {
+    const PASSWORD_MIN_LENGTH = 6;
+    const { email, password } = this.state;
+    return password.length >= PASSWORD_MIN_LENGTH && this.isValidEmail(email);
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <section>
         <h1>Trybe Wallet</h1>
         <form>
           <input
             type="email"
+            name="email"
             data-testid="email-input"
             placeholder="Email"
+            value={ email }
+            onChange={ this.handleChange }
           />
           <input
             type="password"
+            name="password"
             data-testid="password-input"
             placeholder="Senha"
+            value={ password }
+            onChange={ this.handleChange }
           />
           <input
             type="submit"
             value="Entrar"
+            disabled={ !this.isValidLogin() }
           />
         </form>
       </section>
