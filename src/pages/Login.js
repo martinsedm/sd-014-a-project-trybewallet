007162268ee11811e2e29544dc2a0871ddd5bc9e
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-10-15 15:20:17 -0300

    continuando a preparar os arquivos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,22 +10,40 @@ class Login extends React.Component {
       password: '',
       disabledButton: true,
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick() {
+
   }
 
   render() {
     const { email, password, disabledButton } = this.state;
     return (
       <div>
+        Faça seu login
         <div>
           <input
             type="email"
+            name="email"
             placeholder="email"
+            data-testid="email-input"
             value={ email }
             onChange={ this.handleChange }
           />
           <input
             type="password"
+            name="password"
             placeholder="senha"
+            data-testid="password-input"
             value={ password }
             onChange={ this.handleChange }
           />
