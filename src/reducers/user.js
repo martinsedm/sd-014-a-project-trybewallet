58066f91e4b94222974de425a8950d6ab6e16247
MuriloM <muriloommaia@gmail.com>
2021-10-15 15:06:46 -0300

    atualizaçao de estado global

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 1:
+
+    break;
+
+  default:
+    return state;
+  }
+};
+
+export default user;
