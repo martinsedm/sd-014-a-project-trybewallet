7201adb865b41147f76e884d4b785131fd778f86
brunofeu <brunofeu89@gmail.com>
2021-10-15 16:26:28 -0300

    add requirement 06

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,7 +3,45 @@ import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div><Header /></div>;
+    return (
+      <div>
+        <Header />
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input id="value" type="text" name="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input id="description" type="text" name="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda:
+            <select id="currency" type="select" name="currency">
+              <option>moeda</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento:
+            <select id="payment" name="payment">
+              <option>Dinheiro</option>
+              <option>Cartão de Crédito</option>
+              <option>Cartão de Débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag:
+            <select id="tag" name="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
