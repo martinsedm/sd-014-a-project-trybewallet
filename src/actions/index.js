4c56940df06dbc5273cd3185521318e5c3e4c7fe
Douglas Souza <douglassouzabjj@hotmail.com>
2021-10-15 15:49:38 -0300

    c2 - Requisito 3 - Muda para a pagina Wallet e salva o email no estado global ao clicar no botao Entrar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+
+export const makeLogin = (email) => ({
+  type: LOGIN,
+  payload: email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { makeLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      isEmailValid: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  handleClick() {
+    const { history, setMakeLogin } = this.props;
+    const { email } = this.state;
+
+    setMakeLogin(email);
+
+    history.push('/carteira');
+  }
+
   render() {
+    const { email, password, isEmailValid } = this.state;
+    const MIN_LENGTH = 6;
+
     return (
       <div>
         <form>
@@ -9,17 +41,38 @@ class Login extends React.Component {
             data-testid="email-input"
             type="text"
             name="email"
+            onChange={ this.handleChange }
+            value={ email }
           />
           <input
             data-testid="password-input"
             type="password"
             name="password"
+            onChange={ this.handleChange }
+            value={ password }
           />
-          <button type="button"> Entrar </button>
+          <button
+            type="button"
+            disabled={ password.length < MIN_LENGTH && !isEmailValid }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  setMakeLogin: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  setMakeLogin: (email) => dispatch(makeLogin(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
