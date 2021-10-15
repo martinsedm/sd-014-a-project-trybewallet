0a442ebea36772cdaba85fd96857e7b759ec5da9
Monnylly <monnylly@gmail.com>
2021-10-15 19:23:12 -0300

    requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+
+export const saveStateLogin = (payload) => ({
+  type: LOGIN,
+  payload,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,85 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { saveStateLogin as saveStateLoginAction } from '../actions';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      habilit: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({ [name]: value }, () => {
+      const { email, password } = this.state;
+      const CARACTER_MIN = 6;
+      const PASSWORD = password.length >= CARACTER_MIN;
+      const VALID_EMAIL = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i.test(email);
+
+      if (PASSWORD && VALID_EMAIL) {
+        this.setState({ habilit: true });
+      } else {
+        this.setState({ habilit: false });
+      }
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, habilit } = this.state;
+    const { saveStateLogin } = this.props;
+    return (
+      <div className="login">
+        Login
+        <label htmlFor="email">
+          Email
+          <input
+            data-testid="email-input"
+            id="email"
+            type="text"
+            name="email"
+            value={ email }
+            placeholder="Email"
+          />
+        </label>
+        <div>
+          <label htmlFor="pass">
+            Password
+            <input
+              data-testid="password-input"
+              type="password"
+              id="pass"
+              placeholder="password"
+              value={ password }
+              onChange={ this.handleChange }
+              required
+            />
+          </label>
+        </div>
+        <button
+          type="submit"
+          desabilit={ !habilit }
+          onClick={ () => saveStateLogin({ email }) }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  saveStateLogin: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  saveStateLogin: (email) => dispatch(saveStateLoginAction(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
