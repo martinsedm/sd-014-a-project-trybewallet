387fa0786109c9e70866bfac73952c41e4ddecbf
MuriloM <muriloommaia@gmail.com>
2021-10-15 14:56:18 -0300

    Criaçao de store, rootReducer, e inserçao de provider

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+// Coloque aqui suas actions
+
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
