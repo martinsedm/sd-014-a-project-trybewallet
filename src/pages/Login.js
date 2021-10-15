e922c25c743c5698e3c30e3e4026e2827b211567
Saulo Lima <saulolima.c@protonmail.com>
2021-10-15 19:37:17 -0300

    Começando o projeto - Requisito 1 passando

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import '../styles/login.css';
 
-class Login extends React.Component {
+export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleLogin() {
+    return null;
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <form className="login-field">
+        <fieldset>
+          <label htmlFor="email">
+            Email:
+            <input
+              data-testid="email-input"
+              onChange={ this.handleChange }
+              type="email"
+              value={ email }
+              id="email"
+              name="email"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              data-testid="password-input"
+              onChange={ this.handleChange }
+              type="password"
+              id="password"
+              name="password"
+              value={ password }
+            />
+          </label>
+        </fieldset>
+        <button
+          type="submit"
+          onClick={ this.handleLogin }
+        >
+          {' '}
+          Entrar
+          {' '}
+
+        </button>
+      </form>
+    );
   }
 }
-
-export default Login;
