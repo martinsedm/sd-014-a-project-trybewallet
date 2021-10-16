dc6b813565124ebc62d47550c4d4bd968475ace6
Jonathan Ferreira <j0n4t@hotmail.com>
2021-10-15 22:12:01 -0300

    Requisito 04 e 05 - Página da Carteira e Header

---
+++ src/components/WalletHeader.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+
+class WalletHeader extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <h1 data-testid="email-field">{email}</h1>
+        <h1 data-testid="total-field">0</h1>
+        <h1 data-testid="header-currency-field">BRL</h1>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+WalletHeader.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(WalletHeader);
