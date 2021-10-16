31851f82aae874316cb3b0e71d3eb514b7f80bd2
vixemari <marianarocha308@gmail.com>
2021-10-16 00:15:30 -0300

    Requesito 1 e 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,63 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    this.setState({ redirect: true });
+  }
+
   render() {
-    return <div>Login</div>;
+    const lengthPassword = 6;
+    const { email, password, redirect } = this.state;
+   // if (redirect) return <Redirect to="/carteira" />;
+    return (
+      <div>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            onChange={ this.handleChange }
+            value={ email }
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            onChange={ this.handleChange }
+            value={ password }
+            placeholder="Senha"
+          />
+          <button
+            type="submit"
+            onClick={ this.handleClick }
+            disabled={ password.length < lengthPassword }
+
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
