2bf79d5e9fc18d57c26cf26b15e90ba76f12bed1
Dayane <dayanebmartins@gmail.com>
2021-10-15 21:13:33 -0300

    Iniciando Projeto Vamos com Tudo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
 // Coloque aqui suas actions
+import React, { Component } from 'react'
+
+export class index extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default index
+
