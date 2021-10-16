78236d75f9449d088ac6122b407745e40e50e817
unamednada <dias.gustavo.dias@gmail.com>
2021-10-16 00:05:09 -0300

    Create input for email and password in login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          data-testid="email-input"
+          placeholder="Email"
+        />
+        <input
+          data-testid="password-input"
+          placeholder="Password"
+        />
+      </div>
+    );
   }
 }
 
