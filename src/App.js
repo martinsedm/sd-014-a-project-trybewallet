b9576c2301f556f1e1a2c2523f0b3f9e87ab3bc1
Maurício Leite <mauricioleite1@gmail.com>
2021-10-15 21:19:30 -0300

    Requisito 02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={ Login } />
+    <Route path="/carteira" component={ Wallet } />
   </Switch>
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => (
-  <div>
-    <input type="email" data-testid="email-input" />
-    <input type="password" data-testid="password-input" />
-    <button type="submit">Entrar</button>
-  </div>
-);
+const Login = () => {
+  const [emailAddress, setEmailAddress] = useState({ address: '', isValid: false });
+  const [password, setPassword] = useState({ password: '', isValid: false });
+
+  const PASSWORD_MIN_LENGTH = 6;
+
+  const handlePassword = ({ target: { value } }) => {
+    setPassword({ password: value, isValid: value.length >= PASSWORD_MIN_LENGTH });
+  };
+
+  const handleEmailAddress = ({ target: { value } }) => {
+    const testEmail = /^[ ]*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})[ ]*$/i;
+    if (testEmail.test(value)) {
+      setEmailAddress({ address: value, isValid: true });
+    } else { setEmailAddress({ isValid: false }); }
+  };
+
+  return (
+    <div>
+      <h3>Login</h3>
+      <input type="email" data-testid="email-input" onChange={ handleEmailAddress } />
+      <input
+        data-testid="password-input"
+        type="password"
+        minLength="6"
+        onChange={ handlePassword }
+        value={ password.password }
+      />
+      <button disabled={ !password.isValid || !emailAddress.isValid } type="submit">
+        Entrar
+      </button>
+    </div>
+  );
+};
 
 export default Login;
