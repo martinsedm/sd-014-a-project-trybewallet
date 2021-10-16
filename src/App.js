0e3f33e9591097932de679901898279d51665578
Maurício Leite <mauricioleite1@gmail.com>
2021-10-15 21:46:55 -0300

    CSS - correção

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 const App = () => (
   <Switch>
     <div className="container">
-      <Route exact path="/" component={Login} />
-      <Route path="/carteira" component={Wallet} />
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
     </div>
   </Switch>
 );
