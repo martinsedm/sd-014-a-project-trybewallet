3880856c060c0da16a4344513bec1861f8688a31
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-18 17:42:35 -0300

    02 - Set Login component state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,42 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <form>
-          <input data-testid="email-input" type="email" placeholder="Email" />
+          <input
+            data-testid="email-input"
+            name="email"
+            onChange={ this.handleChange }
+            placeholder="Email"
+            type="email"
+            value={ email }
+          />
           <input
             data-testid="password-input"
-            type="password"
+            name="password"
+            onChange={ this.handleChange }
             placeholder="Senha"
+            type="password"
+            value={ password }
           />
           <button type="button">Entrar</button>
         </form>
