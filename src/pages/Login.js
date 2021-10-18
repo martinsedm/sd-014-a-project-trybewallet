2f202eb49f82f04be9cfc97cd90f3deffefba8e6
Matheus Pozett <maa_souza11@hotmail.com>
2021-10-18 15:45:48 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <h1>Trybe Wallet</h1>
+        <form>
+          <input
+            type="email"
+            placeholder="Email"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            data-testid="password-input"
+          />
+          <input
+            type="submit"
+            value="Entrar"
+          />
+        </form>
+      </section>
+    );
   }
 }
 
