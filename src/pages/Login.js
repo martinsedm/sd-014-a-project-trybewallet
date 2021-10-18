6f12e21c135acf0ed1461dfe7446ad0ffdd08175
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-10-18 15:40:31 -0300

    Requisito 2 e 3(incompleto)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   constructor() {
@@ -18,35 +19,40 @@ class Login extends React.Component {
 
   render() {
     const { email, password } = this.state;
+    const numberMinCaracteres = '6';
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+/i;
     return (
       <fieldset>
         <label htmlFor="login">
           <input
             id="login"
             data-testid="email-input"
-            type="email"
+            type="text"
             onChange={ this.handleChange }
-            value={ email }
             name="email"
+            value={ email }
             placeholder="Digite seu email"
             required
           />
           <input
             id="login"
             data-testid="password-input"
-            type="text"
+            type="password"
             onChange={ this.handleChange }
-            value={ password }
             name="password"
+            value={ password }
             placeholder="Digite sua senha"
             required
           />
-          <button
-            type="button"
-            disabled
-          >
-            Entrar
-          </button>
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ !emailRegex.test(email)
+                 || password.length < numberMinCaracteres }
+            >
+              Entrar
+            </button>
+          </Link>
         </label>
       </fieldset>
     );
