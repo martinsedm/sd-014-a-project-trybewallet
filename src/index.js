7211584286c76c4ae834c1311d4102d5fc1bb612
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-10-18 12:55:48 -0300

    Retira por hora a tag Provider

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store/store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
