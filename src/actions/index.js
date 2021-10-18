bc3377ddd55e486973521f20d170deb19bb64ab2
Andrea <andrealh08@gmail.com>
2021-10-18 19:11:59 -0300

    Estrutura redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,4 @@
 // Coloque aqui suas actions
+export const SET_INFO = 'SET_INFO';
+export const SET_EXPENSES = 'SET_EXPENSES';
+export const SET_CURRENCIES = 'SET_CURRENCIES';
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,26 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { SET_EXPENSES, SET_CURRENCIES } from '../actions';
+
+const INITIAL_STATE = {
+  expenses: [],
+  currencies: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SET_EXPENSES:
+    return {
+      ...state,
+      expenses: action.payload,
+    };
+  case SET_CURRENCIES:
+    return {
+      ...state,
+      currencies: action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
