1824422836874950670f894728ee3d58b3bef10c
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-10-18 12:54:37 -0300

    Criaçao da pagina de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="text" />
+        <input type="password" />
+        <button type="submit">Entrar</button>
+      </div>
+    );
   }
 }
 
