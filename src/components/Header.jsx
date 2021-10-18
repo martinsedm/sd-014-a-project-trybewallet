861e82f390e27ed1f2bac58ba022361c63fc7fb2
Écio Ferraz <ecioferraz@hotmail.com>
2021-10-18 15:23:07 -0300

    req 8

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,30 @@ import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
-    const { email } = this.props;
+    const { email, expenses } = this.props;
+    const totalExpenses = expenses.reduce((total, { value, exchangeRates, currency }) => {
+      total += value * exchangeRates[currency].ask;
+      return total;
+    }, 0);
+
     return (
       <header>
         <p data-testid="email-field">{ email }</p>
-        <p data-testid="total-field">0</p>
+        <p data-testid="total-field">{ (totalExpenses).toFixed(2) }</p>
         <p data-testid="header-currency-field">BRL</p>
       </header>
     );
   }
 }
 
-const mapStateToProps = ({ user: { email } }) => ({
+const mapStateToProps = ({ user: { email }, wallet: { expenses } }) => ({
   email,
+  expenses,
 });
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
