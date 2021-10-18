0f0be69c2dcf25c7c19050eb915e9202c14b68c3
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-10-18 08:13:27 -0300

    Adiciona página de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h2>Trybe Wallet</h2>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="email"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Senha"
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
