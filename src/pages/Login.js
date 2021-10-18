9bdb183490659d11ad481f03a5952b62824fa6fa
Lucas Rimis <lucasrimis@gmail.com>
2021-10-18 17:24:39 -0300

    Requisitos 1 e 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,51 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const minPasswordLength = 6;
+
+    return (
+      <div>
+        <input
+          type="email"
+          value={ email }
+          name="email"
+          data-testid="email-input"
+          placeholder="Digite seu email"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          value={ password }
+          name="password"
+          data-testid="password-input"
+          placeholder="Digite sua senha"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={
+            (!email.includes('@' && '.com') || password.length < minPasswordLength)
+          }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_USER_STATE = {
+  email: '',
+  password: '',
+};
+
+const user = (
+  state = INITIAL_USER_STATE,
+  action,
+) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default user;
