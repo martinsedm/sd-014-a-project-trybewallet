4dd3e62acdf93fcda2f553b6b9e8c2385ebfef77
User <gabrielkleber006@gmail.com>
2021-10-18 16:38:53 -0300

    test requisito 1.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <input type="email" placeholder="email" data-testid="email-input" />
+          <input type="password" placeholder="password" data-testid="password-input" />
+          <button type="button">Entrar</button>
+        </form>
+      </div>);
   }
 }
 
