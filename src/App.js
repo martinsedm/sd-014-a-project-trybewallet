75aa09991de3b05e68529bac03bc711832d70abe
Matheus Pozett <maa_souza11@hotmail.com>
2021-10-18 16:23:45 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Login />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/carteira" component={ Wallet } />
+        </Switch>
       </div>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,53 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
+  }
+
+  isValidEmail(email) { return email.match(/.+@.+\..+/); }
+
+  isValidLogin() {
+    const MIN_CARACTERS_PASSWORD = 6;
+    const { email, password } = this.state;
+    return password.length >= MIN_CARACTERS_PASSWORD && this.isValidEmail(email);
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <section>
         <h1>Trybe Wallet</h1>
         <form>
           <input
             type="email"
+            name="email"
             placeholder="Email"
             data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
           />
           <input
             type="password"
+            name="password"
             placeholder="Senha"
             data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
           />
           <input
             type="submit"
             value="Entrar"
+            disabled={ !this.isValidLogin() }
           />
         </form>
       </section>
