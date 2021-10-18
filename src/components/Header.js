0b3d2fc21d564bd0a68c4ea8048fd0204d69194c
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-10-18 19:02:24 -0300

    requisito 9 ok... e tentando entender como passar nos outros requisitos...

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
-    const { data: { email, total, currency } } = this.props;
+    const { data: { email, total } } = this.props;
     return (
       <header>
         <p>
@@ -16,7 +16,7 @@ class Header extends React.Component {
           {' '}
           <span data-testid="total-field">{ total }</span>
           {' '}
-          <span data-testid="header-currency-field">{ currency }</span>
+          <span data-testid="header-currency-field">BRL</span>
         </p>
       </header>
     );
@@ -28,7 +28,7 @@ Header.propTypes = {
     {
       email: PropTypes.string.isRequired,
       total: PropTypes.number.isRequired,
-      currency: PropTypes.string.isRequired,
+      currencyExchange: PropTypes.string.isRequired,
     },
   ).isRequired,
 };
