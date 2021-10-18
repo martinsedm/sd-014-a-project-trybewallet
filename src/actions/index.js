4793585066d4ed317d5a9baaee0dca07b11e0f2c
JosueTavaress <josueferreira8.jf@gmail.com>
2021-10-18 18:27:48 -0300

    add requisito 1-2 component -> Login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
-// Coloque aqui suas actions
+export const ADD_EMAIL = 'ADD_EMAIL';
+
+function addEmail(email) {
+  return {
+    type: ADD_EMAIL,
+    email,
+  };
+}
+
+export default addEmail;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,71 @@
 import React from 'react';
+import '../styles/login.css';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handlechange = this.handlechange.bind(this);
+  }
+
+  handlechange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  enableBtn(email, password) {
+    const minimumNumber = 6;
+
+    // https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
+    // regex -> author:Matheus Battisti
+    // /^((?!.)[\w-_.]*[^.])(@\w+)(.\w+(.\w+)?[^.\W])$/gm; ----- /\S+@\S+\.\S+/
+    const regex = /\S+@\S+\.\S+/;
+
+    const validationEmail = regex.test(email);
+    if (validationEmail && password.length >= minimumNumber) return true;
+    return false;
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form className="containner">
+        <div className="containner border">
+          <div>
+            <img className="logo" src="/img/try.png" alt="logo_trybe_wallat" />
+            <img className="imgWallat" src="/img/wallat.jpg" alt="img wallat" />
+          </div>
+          <h2 className="textLogo">Wallet</h2>
+          <input
+            placeholder="Email"
+            className="inputEmail"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            type="email"
+            onChange={ this.handlechange }
+          />
+          <input
+            placeholder="Senha"
+            className="inputPassword"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            type="password"
+            onChange={ this.handlechange }
+          />
+          <button
+            className={ this.enableBtn(email, password) ? 'btnActivated' : 'btnDisabled' }
+            disabled={ !this.enableBtn(email, password) }
+            type="button"
+          >
+            Entrar
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 
