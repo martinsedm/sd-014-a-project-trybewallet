506910b61005b06fb966ed1835e1670f92792a9a
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-10-18 16:55:49 -0300

    Fazendo validaçao de email e senha do formulario de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,55 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
+    const MAX_LEN = 6;
     return (
       <div>
-        <input type="text" data-testid="email-input" />
-        <input type="password" data-testid="password-input" />
-        <button type="submit">Entrar</button>
+        <input
+          name="email"
+          type="email"
+          data-testid="email-input"
+          onChange={ this.handleChange }
+        />
+        <input
+          name="password"
+          type="password"
+          data-testid="password-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          className="button"
+          type="submit"
+          disabled={
+            !email
+            || !password
+            || email.split('.').includes('com') === false
+            || (password.length >= MAX_LEN) === false
+          }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
