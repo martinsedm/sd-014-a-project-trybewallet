6ae199b7ae5ccc70d57062d620562dea30781f89
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-10-18 14:50:39 -0300

    Colocando datatest id no input do email e senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,8 +4,8 @@ class Login extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" />
-        <input type="password" />
+        <input type="text" data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
         <button type="submit">Entrar</button>
       </div>
     );
