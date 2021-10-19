373634aced6e921ccf1384b932d3153f668b48b8
Ramon Vitor <vitorramon@hotmail.com>
2021-10-19 06:34:51 -0300

    Requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">{ email }</span>
+        <span data-testid="total-field">{ `Dispensa total: ${0} ` }</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    );
   }
 }
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
 
 export default Wallet;
