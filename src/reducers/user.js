2e5814f4cbc0943ee116bc2020df63549e8ec799
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-10-19 15:35:07 -0300

    configurando o store, rootReducer e reducers e o provider

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INICIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+function user(state = INICIAL_STATE, action) {
+  switch (action.type) {
+  case 1:
+    return {};
+  default:
+    return state;
+  }
+}
+
+export default user;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INICIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+function wallet(state = INICIAL_STATE, action) {
+  switch (action.type) {
+  case 1:
+    return {};
+  default:
+    return state;
+  }
+}
+
+export default wallet;
