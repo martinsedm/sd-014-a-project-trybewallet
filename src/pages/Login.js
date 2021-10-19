f8d683b134465508b44be682115606ddb4b3095f
Ramon Vitor <vitorramon@hotmail.com>
2021-10-19 05:46:53 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      senha: '',
+      validation: true,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleValidation = this.handleValidation.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => this.handleValidation());
+  }
+
+  handleValidation() {
+    const MIN_LENGTH = 6;
+    const { email, senha } = this.state;
+    const validation = !(/\w+@\w+.com/.test(email) && senha.length >= MIN_LENGTH);
+    this.setState({ validation });
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/wallet');
+  }
+
   render() {
+    const { email, senha, validation } = this.state;
     return (
-      <form>
+      <form onSubmit={ this.handleSubmit }>
         <label htmlFor="email">
           Email:
           <input
             type="text"
             data-testid="email-input"
             name="email"
+            value={ email }
+            onChange={ this.handleChange }
           />
         </label>
 
-        <label htmlFor="password">
+        <label htmlFor="senha">
           Senha:
           <input
             type="password"
             data-testid="password-input"
-            name="password"
+            name="senha"
+            value={ senha }
+            onChange={ this.handleChange }
           />
         </label>
 
         <button
           type="button"
+          disabled={ validation }
+          onClick={ this.handleClick }
         >
           Entrar
         </button>
@@ -31,5 +73,12 @@ class Login extends React.Component {
     );
   }
 }
+
+Login.propTypes = {
+  validEmail: PropTypes.func,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+}.isRequired;
 
 export default Login;
