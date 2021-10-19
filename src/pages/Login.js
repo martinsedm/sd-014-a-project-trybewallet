42c8f056fa3a3eda173aa8fd52db6d13f82e2c9b
Anna Hamann <annasoldih@gmail.com>
2021-10-19 14:49:59 -0300

    creates login inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <header>
+          <h1>TrybeWallet by Anna Hamann</h1>
+          <h3>
+            Login
+          </h3>
+        </header>
+        <form>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            data-testid="password-input"
+          />
+          <button
+            type="submit"
+          >
+            Entrar
+          </button>
+        </form>
+      </main>
+    );
   }
 }
 
