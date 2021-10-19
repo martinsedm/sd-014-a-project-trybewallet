0919f8b9177c576e170048ef2ba059049ddf5b90
Viviane Freitas <satierfviviane@gmail.com>
2021-10-19 16:34:26 -0300

    Requisito 1 - Refeito e OK

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SAVE_PASSWORD = 'SAVE_PASSWORD';
 
 export const saveEmail = (payload) => ({
   type: SAVE_EMAIL,
+  payload,
+});
+
+export const savePassword = (payload) => ({
+  type: SAVE_PASSWORD,
   payload,
 });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'; // refazendo tudo sem desespero pra aprender
 import { connect } from 'react-redux';
-import { saveEmail as saveEmailAction } from '../actions';
+import PropTypes from 'prop-types';
+import InputLogin from '../components/InputLogin';
+import Button from '../components/Button';
 
 class Login extends React.Component {
   constructor() {
@@ -11,75 +12,54 @@ class Login extends React.Component {
       password: '',
     };
     this.handleChange = this.handleChange.bind(this);
-    this.validateEmail = this.validateEmail.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange({ target: { name, value } }) {
-    this.setState(
-      { [name]: value },
-    );
-  }
-
-  // validate email reference: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-  validateEmail() {
-    const { email } = this.state;
-    const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailValidation.test(email);
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   handleClick() {
     const { email } = this.state;
-    const { saveEmail, history } = this.props;
-    saveEmail({ email });
+    const { changeInput, history } = this.props;
+    changeInput(email);
     history.push('./carteira');
   }
 
   render() {
     const { email, password } = this.state;
-    const MIN_CHARACTERS_PASSWORD = 6;
-    const passwordLength = password.length >= MIN_CHARACTERS_PASSWORD;
     return (
-      <form>
-        <label htmlFor="email-input">
-          <input
-            data-testid="email-input"
-            type="email"
-            name="email"
-            value={ email }
-            placeholder="digite seu e-mail"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="password-input">
-          <input
-            data-testid="password-input"
-            type="password"
-            name="password"
-            value={ password }
-            placeholder="digite sua senha"
-            minLength="6"
-          />
-        </label>
-        <button
-          type="button"
-          disabled={ !passwordLength && !(this.validateEmail()) }
-          onClick={ this.handleClick }
-        >
-          Entrar
-        </button>
-      </form>
+      <div>
+        <InputLogin
+          label="E-mail"
+          name="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ this.handleChange }
+          placeholder="Digite seu e-mail"
+        />
+        <InputLogin
+          label="Password"
+          name="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ this.handleChange }
+          placeholder="Digite sua senha"
+        />
+        <Button onClick={ this.handleClick } />
+      </div>
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  saveEmail: (payload) => dispatch(saveEmailAction(payload)),
+  changeInput: (state) => dispatch(SAVE_EMAIL(state)),
 });
 
 Login.propTypes = {
-  history: PropTypes.objectOf(PropTypes.any).isRequired,
-  saveEmail: PropTypes.func.isRequired,
+  changeInput: PropTypes.func.isRequired,
+  history: PropTypes.shape().isRequired,
 };
 
 export default connect(null, mapDispatchToProps)(Login);
