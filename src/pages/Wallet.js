f358462a5af6e2e5373d0c98d52b9f99c55c3c87
vixemari <marianarocha308@gmail.com>
2021-10-19 15:40:20 -0300

    Requesito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../Components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <main>
+        <Header />
+      </main>
+    );
   }
 }
-
 export default Wallet;
