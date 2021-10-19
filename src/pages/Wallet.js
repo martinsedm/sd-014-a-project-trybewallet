c22f5c71bc82a85e3aa9ba86352aa483c4239bc4
Monnylly <monnylly@gmail.com>
2021-10-19 11:41:03 -0300

    correcao lint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import fetchApi from '../components/FetchApi';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import fetchApi from '../components/FetchApi';
 import FormInputs from '../components/FormInputs';
 import Header from '../components/Header';
 
