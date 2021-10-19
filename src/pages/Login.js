fd7800d2baa3a4fa0200873d1c8054e43de28bea
Caio César Amorim <caiocavalcante063@gmail.com>
2021-10-19 10:10:08 -0300

    cumpre requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      validateEmail: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  handleChange({ target: { value, id } }) {
+    this.setState({
+      [id]: value,
+    }, () => {
+      const { email } = this.state;
+      const enableEmail = this.validateEmail(email);
+      this.setState({ validateEmail: enableEmail });
+    });
+  }
+
   render() {
+    const { email, password, validateEmail } = this.state;
+    const MIN_PASSWORD_CHARACTERS = 6;
+
     return (
       <form>
         <label htmlFor="email">
@@ -10,6 +39,9 @@ class Login extends React.Component {
             type="email"
             id="email"
             data-testid="email-input"
+            onChange={ this.handleChange }
+            value={ email }
+            required
           />
         </label>
         <br />
@@ -19,15 +51,21 @@ class Login extends React.Component {
             type="password"
             id="password"
             testid="password-input"
+            onChange={ this.handleChange }
           />
         </label>
         <br />
-        <button
-          data-testid="password-input"
-          type="button"
+        <Link
+          to="/carteira"
         >
-          Entrar
-        </button>
+          <button
+            data-testid="password-input"
+            type="button"
+            disabled={ !(password.length >= MIN_PASSWORD_CHARACTERS && validateEmail) }
+          >
+            Entrar
+          </button>
+        </Link>
       </form>
     );
   }
