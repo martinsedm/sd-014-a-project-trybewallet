a6528759d829a51fead85eb48c25d3265777c31d
Caio César Amorim <caiocavalcante063@gmail.com>
2021-10-19 16:31:39 -0300

    cumpre requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <>
+        <Header />
+        Wallet
+      </>
+    );
   }
 }
