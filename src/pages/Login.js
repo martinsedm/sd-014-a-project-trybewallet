bd44fe5f6f9e2dd56ef9e23e464e85df038a48f2
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-10-19 10:52:54 -0300

    testando requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input
+          data-testid="email-input"
+          placeholder="Digite seu email"
+        />
+        <input
+          data-testid="password-input"
+          placeholder="Digite sua senha"
+        />
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
