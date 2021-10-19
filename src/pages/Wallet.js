a1bc28a96826579edffddba2a86bcd6299b3c8ae
Samuel Nascimento <sfsnascimento@gmail.com>
2021-10-19 14:46:12 -0300

    Requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userLogged } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">{ userLogged }</span>
+        { ' ' }
+        <span data-testid="total-field">0</span>
+        { ' ' }
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userLogged: state.user.email,
+});
+
+Wallet.propTypes = ({
+  userLogged: PropTypes.string.isRequired,
+});
+
+export default connect(mapStateToProps)(Wallet);
