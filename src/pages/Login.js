7e427c4f0a76427cb6f26223ec72e8120e852232
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-10-19 15:52:12 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="Email"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          data-testid="password-input"
+        />
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
