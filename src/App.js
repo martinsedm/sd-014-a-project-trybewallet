9d64bcd3dfe870d18d71a46e6adaa91c838fb209
vanessabastos <vanessarsb83@gmail.com>
2021-10-19 17:17:49 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import Login from './pages/Login';
+import store from './store';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <div>Hello, TrybeWallet!</div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+
+export const emailSenha = () => ({
+  type: LOGIN,
+  payload,
+});
