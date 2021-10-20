dc16934aa92f27ed294fc9870a5c9642b2b09cc3
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-10-20 16:26:19 -0300

    Cria uma pagina inicial - requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="login-email">
+          <input
+            type="email"
+            name="email"
+            id="login-email"
+            data-testid="email-input"
+            placeholder="Insira seu email aqui..."
+          />
+        </label>
+        <label htmlFor="login-password">
+          <input
+            type="text"
+            id="login-password"
+            name="password"
+            data-testid="password-input"
+            placeholder="Insira sua senha"
+          />
+        </label>
+        <label htmlFor="login-btn-submit">
+          <input
+            type="submit"
+            id="login-btn-submit"
+            value="Entrar"
+          />
+        </label>
+      </form>
+    );
   }
 }
 
