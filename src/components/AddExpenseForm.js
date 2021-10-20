35e5bac435450922e27cd84b6080fbba578bd515
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-20 06:14:11 -0300

    07 - Search currencies from API

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,46 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function AddExpenseForm() {
-  return (
-    <div>
-      <h1>Cadastro de despesas</h1>
-      <form>
-        <label htmlFor="value">
-          Valor
-          <input type="text" id="value" />
-        </label>
-        <label htmlFor="description">
-          Descrição
-          <input type="text" id="description" />
-        </label>
-        <label htmlFor="currency">
-          Moeda
-          <select id="currency">
-            <option>Real</option>
-            <option>Dolar</option>
-            <option>Euro</option>
-          </select>
-        </label>
-        <label htmlFor="paymentMethod">
-          Método de pagamento
-          <select id="paymentMethod">
-            <option>Dinheiro</option>
-            <option>Cartão de crédito</option>
-            <option>Cartão de débito</option>
-          </select>
-        </label>
-        <label htmlFor="category">
-          Tag
-          <select id="category">
-            <option>Alimentação</option>
-            <option>Lazer</option>
-            <option>Trabalho</option>
-            <option>Transporte</option>
-            <option>Saúde</option>
-          </select>
-        </label>
-        <button type="button">Adicionar despesa</button>
-      </form>
-    </div>
-  );
+class AddExpenseForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currencies: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const data = await response.json();
+    const currencies = Object.keys(data).filter(
+      (currency) => currency !== 'USDT',
+    );
+    this.updateState({ currencies });
+  }
+
+  updateState(state) {
+    this.setState(state);
+  }
+
+  render() {
+    const { currencies } = this.state;
+    return (
+      <div>
+        <h1>Cadastro de despesas</h1>
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input type="text" id="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input type="text" id="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select id="currency">
+              {currencies.map((currency) => (
+                <option key={ currency } value={ currency }>
+                  {currency}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="paymentMethod">
+            Método de pagamento
+            <select id="paymentMethod">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="category">
+            Tag
+            <select id="category">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          <button type="button">Adicionar despesa</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default AddExpenseForm;
