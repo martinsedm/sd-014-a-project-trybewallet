0d40ef49636c438f712f9f093be71b4542643321
Fernando F Monteiro <fernandoflmbatera@gmail.com>
2021-10-20 15:44:12 -0300

    Página de Login - Requisito 01 - E-mail e Senha

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+export default function App() {
+  return (
+    <>
+      <div>Hello, TrybeWallet!</div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/wallet" component={ Wallet } />
+      </Switch>
+    </>
+  );
 }
-
-export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import  React, { Component } from 'react';
 
-class Login extends React.Component {
+export default class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <form>
+          <label for="email">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value=""
+              data-testid="email-input"
+            />
+          </label>
+          <label for="password">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value=""
+              data-testid="password-input"
+            />
+          </label>
+          <button
+            type="button"
+            // onClick={this.}
+            // disabled={this.props.}
+          >
+            Entrar
+          </button>
+        </form>
+      </ >
+    );
   }
 }
-
-export default Login;
