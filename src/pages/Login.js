53c1cd08ef8c424f363b5996430b6c760d23abe2
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-10-20 18:05:31 -0300

    Verifica validaçoes de email, senha e botao - requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,49 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
+    const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const MIN_CHARACTHERS = 6;
     return (
       <form>
         <label htmlFor="login-email">
           <input
             type="email"
             name="email"
-            id="login-email"
             data-testid="email-input"
+            id="login-email"
+            onChange={ this.handleChange }
+            value={ email }
             placeholder="Insira seu email aqui..."
+            required
           />
         </label>
         <label htmlFor="login-password">
           <input
             type="text"
+            data-testid="password-input"
             id="login-password"
             name="password"
-            data-testid="password-input"
+            onChange={ this.handleChange }
+            value={ password }
             placeholder="Insira sua senha"
+            required
           />
         </label>
         <label htmlFor="login-btn-submit">
@@ -27,6 +51,8 @@ class Login extends React.Component {
             type="submit"
             id="login-btn-submit"
             value="Entrar"
+            // REFERENCIA DE VALIDAÇÃO REGEX = https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+            disabled={ !EMAIL_REGEX.test(email) || password.length < MIN_CHARACTHERS }
           />
         </label>
       </form>
