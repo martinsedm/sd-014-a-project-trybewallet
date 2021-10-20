11ba018bceb98f37f86468212551b0942c66a811
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-10-20 19:55:09 -0300

    Cria header - requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,28 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <div>
+          <h4 data-testid="email-field">{`Email: ${email}`}</h4>
+          <h4 data-testid="total-field">0</h4>
+          <h4 data-testid="header-currency-field">BRL</h4>
+        </div>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = ({ user }) => ({
+  email: user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
