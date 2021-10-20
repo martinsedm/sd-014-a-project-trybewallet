ef49e76c0c2dae0f9ed26b82ff61ebb28e126ce5
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-10-20 10:33:30 -0300

    Montagem de actions para requisiçao API currency

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,28 @@
 // Coloque aqui suas actions
 
-const guardaEmail = (payload) => ({
+export const guardaEmail = (payload) => ({
   type: 'EMAIL',
   payload,
 });
 
-export default guardaEmail;
+export const REQUEST_MOEDAS = 'REQUEST_MOEDAS';
+
+export const RECEIVE_MOEDAS = 'RECEIVE_MOEDAS';
+
+const requestMoedas = () => ({
+  type: REQUEST_MOEDAS,
+});
+
+const receiveMoedas = (moedas) => ({
+  type: RECEIVE_MOEDAS,
+  moedas,
+});
+
+export function fetchMoedas() {
+  return (dispatch) => {
+    dispatch(requestMoedas());
+    return fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((moedas) => dispatch(receiveMoedas(moedas)));
+  };
+}
