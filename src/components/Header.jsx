983a7d0297ff1839543ec28e32ad748baafcbf0a
Heitor SF <heitor.sf5@gmail.com>
2021-10-21 01:40:14 -0300

    the guesswork on eslint continues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import currencyXchanger from './CurrencyExchange';
 
-
 class Header extends React.Component {
   constructor(props) {
     super(props);
