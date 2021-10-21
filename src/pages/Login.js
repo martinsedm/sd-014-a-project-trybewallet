9ce26981ded19b94df15249fdde9be4e783200bd
BeatrizR <biarl.ramalhorl@gmail.com>
2021-10-20 21:16:17 -0300

    requisto 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { email, value } }) {
+    this.setState({ [email]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <label htmlFor="email">
+          <input
+            data-testid="email-input"
+            placeholder="Email"
+            type="text"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            placeholder="Digite sua senha"
+            value={ password }
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </form>
+
+    );
   }
 }
 
