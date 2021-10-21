498083e9488e13ec0409d6c0037045a6b73ba7ed
Heitor SF <heitor.sf5@gmail.com>
2021-10-21 04:29:45 -0300

    properly commentated the newId variable

--- src/components/ExpenseManager.js
+++ src/components/ExpenseManager.js
@@ -1,9 +1,9 @@
 const filterXpenses = (expenses, id) => {
-  let newId = 0;
+  // let newId = 0;
   const filtered = expenses.filter((xpense) => {
     if (parseInt(xpense.id, 10) !== parseInt(id, 10)) {
       // xpense.id = newId;
-      newId += 1;
+      // newId += 1;
       return xpense;
     }
     return false;
