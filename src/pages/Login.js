4448dacebdbf63e880663dc034573a49dfad10c4
jmporto <jmporto@outlook.com>
2021-10-21 00:11:06 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,38 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  enableButton() {
+    const { password } = this.state;
+    const MIN_LENGTH = 6;
+    if (password.length < MIN_LENGTH) return true;
+    if (!this.emailValidation()) return true;
+    return false;
+  }
+
+  // https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+  emailValidation() {
+    const { email } = this.state;
+    const emailValidation = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    return emailValidation.test(email);
+  }
+
   render() {
     return (
       <div>
@@ -16,8 +48,14 @@ class Login extends React.Component {
           name="password"
           placeholder="password"
           data-testid="password-input"
+          onChange={ this.handleChange }
         />
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          disabled={ this.enableButton() }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
