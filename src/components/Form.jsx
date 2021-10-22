07aaecaa6ad2e2fb68a0358a4aecb53749667d79
Fernando F Monteiro <fernandoflmbatera@gmail.com>
2021-10-22 16:17:22 -0300

    Requisito 06

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,42 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
 export default class Form extends Component {
   render() {
     return (
       <form>
-        <label>
+        <label htmlFor="value">
           Valor
-          <input type="text" />
+          <input type="text" id="value" name="value" />
         </label>
-        <label>
+        <label htmlFor="description">
           Descrição
-          <input type="text" />
+          <input type="text" id="description" name="description" />
         </label>
-        <label>
+        <label htmlFor="currency">
           Moeda
-          <select name="" id=""></select>
+          <select name="currency" id="currency">
+            <option value="deleteme">deleteme</option>
+          </select>
         </label>
-        <label>
+        <label htmlFor="payment">
           Método de Pagamento
-          <select name="" id=""></select>
+          <select name="payment" id="payment">
+            <option value="dinheiro">Dinheiro</option>
+            <option value="credito">Cartão de crédito</option>
+            <option value="debito">Cartão de débito</option>
+          </select>
         </label>
-        <label>
+        <label htmlFor="tag">
           Tag
-          <select name="" id=""></select>
+          <select name="tag" id="tag">
+            <option value="alimentacao">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="transporte">Transporte</option>
+            <option value="saude">Saúde</option>
+          </select>
         </label>
       </form>
     );
