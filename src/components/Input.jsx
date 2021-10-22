aa5a78e6ebd8d414adb0acd6c2808cbc6f70ce2a
Vincenzofdg <vincenzofdg@hotmail.com>
2021-10-22 17:22:46 -0300

    Segundo Requisito

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 
 class Input extends Component {
   render() {
-    const { id, label } = this.props;
+    const { id, tag, func, value } = this.props;
     return (
       <label htmlFor={ id }>
-        {`${label}: `}
+        {`${tag}: `}
         <input
           id={ id }
           name={ id }
           type={ id }
+          value={ value }
+          onChange={ func }
           data-testid={ `${id}-input` }
         />
       </label>
@@ -20,7 +22,9 @@ class Input extends Component {
 
 Input.propTypes = {
   id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  func: PropTypes.func.isRequired,
 };
 
 export default Input;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,40 @@ import React, { Component } from 'react';
 import Input from '../components/Input';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.emailValidation = this.emailValidation.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  emailValidation(email) {
+    const i = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    return i.test(String(email).toLowerCase());
+  }
+
   render() {
+    const { email, password } = this.state;
+    const MIN = 6;
     return (
       <fieldset>
-        <Input id="email" label="Email" />
-        <Input id="password" label="Senha" />
-        <button type="submit">Entrar</button>
+        <Input id="email" tag="Email" value={ email } func={ this.handleChange } />
+        <Input id="password" tag="Senha" value={ password } func={ this.handleChange } />
+        <button
+          type="submit"
+          disabled={ password.length < MIN || !this.emailValidation(email) }
+        >
+          Entrar
+        </button>
       </fieldset>
     );
   }
