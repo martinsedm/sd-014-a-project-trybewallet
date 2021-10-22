d2c36e2cdaf5ce81f713d1375a480fe802c911fa
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-10-22 17:34:36 -0300

    finalizando requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,7 +5,19 @@ import PropTypes from 'prop-types';
 class Wallet extends React.Component {
   render() {
     const { email } = this.props;
-    return <div>{email}</div>;
+    const valorInicial = 0;
+    const selectedCurrency = 'BRL';
+    return (
+      <main>
+        <h2 data-testid="email-field">{email}</h2>
+        <h3 data-testid="total-field">{`Despesa total Gerada: R$ ${valorInicial}`}</h3>
+        <h3
+          data-testid="header-currency-field"
+        >
+          {`Cambio selecionado: ${selectedCurrency}`}
+        </h3>
+      </main>
+    );
   }
 }
 
