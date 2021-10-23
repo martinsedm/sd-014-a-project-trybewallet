59e813fc244f9bfbb8a442561d17abdd891c412d
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-10-23 18:18:03 -0300

    removendo label e select do componente Currencies

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import LabelCurrencies from './LabelCurrencies';
+import Currencies from './Currencies';
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      description: '',
+      currency: '',
+      payment: '',
+      tag: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { id, value }}) {
+    this.setState({ [id]: value });
+  }
+
   render() {
+    const { value, description, currency, payment, tag } = this.state;
     return (
       <form>
         <label htmlFor="value">
           Valor
-          <input
-            type="number"
-            id="value"
-          />
+          <input type="text" id="value" value={ value } onChange={ this.handleChange } />
         </label>
         <label htmlFor="description">
           Descrição
           <input
             type="text"
             id="description"
+            value={ description }
+            onChange={ this.handleChange }
           />
         </label>
-        <LabelCurrencies />
+        <label htmlFor="currency">
+          Moeda
+          <select id="currency" value={ currency } onChange={ this.handleChange }>
+            <Currencies />
+          </select>
+        </label>
         <label htmlFor="payment">
           Método de pagamento
-          <select id="payment">
+          <select id="payment" value={ payment } onChange={ this.handleChange }>
             <option value="dinheiro">Dinheiro</option>
             <option value="credito">Cartão de crédito</option>
             <option value="debito">Cartão de débito</option>
@@ -30,7 +51,7 @@ class Form extends Component {
         </label>
         <label htmlFor="tag">
           Tag
-          <select id="tag">
+          <select id="tag" value={ tag } onChange={ this.handleChange }>
             <option value="alimentacao">Alimentação</option>
             <option value="lazer">Lazer</option>
             <option value="trabalho">Trabalho</option>
