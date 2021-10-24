1e0c127f7df8d8e74b95d86bfffe7ce92120fd60
dandara-dias <dandaradias.contato@gmail.com>
2021-10-24 20:35:27 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      validEmail: false,
+      validPassword: false,
+    };
+
+    this.handleVerification = this.handleVerification.bind(this);
+  }
+
+  handleVerification({ target }) {
+    if (target.name === 'email') {
+      // src: https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
+      const regex = /\S+@\S+\.\S+/;
+      const validEmail = regex.test(target.value);
+
+      this.setState({
+        validEmail,
+      });
+    } else {
+      const CARACTERES_SENHA = 6;
+      const validPassword = target.value.length >= CARACTERES_SENHA;
+
+      this.setState({
+        validPassword,
+      });
+    }
+  }
+
   render() {
+    const { validEmail, validPassword } = this.state;
     return (
       <>
         <div>Login</div>
@@ -12,6 +42,7 @@ class Login extends React.Component {
               type="email"
               name="email"
               data-testid="email-input"
+              onChange={ this.handleVerification }
             />
           </label>
 
@@ -21,10 +52,14 @@ class Login extends React.Component {
               type="password"
               name="password"
               data-testid="password-input"
+              onChange={ this.handleVerification }
             />
           </label>
 
-          <button type="button">
+          <button
+            type="button"
+            disabled={ !(validEmail && validPassword) }
+          >
             Entrar
           </button>
         </form>
