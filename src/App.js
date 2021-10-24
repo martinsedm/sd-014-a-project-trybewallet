a4ecd9566ff2682218056b6a7f29a9b32836a0de
jhonathancs <jhonathan_jcs@hotmail.com>
2021-10-24 07:04:54 -0500

    req_01-02
    cheguei chegando vamos com tudo antes tarde do que nuuncaaaaa hahahahah

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import SwitcherRoute from './component/SwitcherRoute';
+import store from './store';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Provider store={ store }>
+          <SwitcherRoute />
+        </Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
