e6f00193fad8fac589b90786a26433ea4ed47291
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-25 11:12:25 -0300

    Resolve requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input
+          type="email"
+          name="email"
+          pĺaceholder="E-mail"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          pĺaceholder="Senha"
+          data-testid="password-input"
+        />
+        <button type="submit">Entrar</button>
+      </div>
+    );
   }
 }
 
