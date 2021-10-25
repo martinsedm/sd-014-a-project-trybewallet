acf2659ab714d38e48c273ad084b1a3b599ad6af
Saulo Lima <saulolima.c@protonmail.com>
2021-10-25 20:51:49 -0300

    hoje nao, lint

--- src/components/FormsComp.js
+++ src/components/FormsComp.js
@@ -9,7 +9,7 @@ class FormsComp extends Component {
     super();
     this.state = {
       value: '',
-      currency: '',
+      currency: 'USD',
       method: 'Dinheiro',
       tag: 'Alimentação',
       description: '',
@@ -53,10 +53,10 @@ class FormsComp extends Component {
             value={ currency }
             onChange={ this.hdlChange }
           >
-            {Object.values(exchangeRates).map((coin, i) => {
-              if (coin.codein !== 'BRLT' && coin.code !== 'DOGE') {
-                return (<option key={ i }>{coin.code}</option>);
-              }
+            { Object.values(exchangeRates).map((coins, i) => {
+              if (coins.codein !== 'BRLT' && coins.code !== 'DOGE') {
+                return (<option key={ i }>{coins.code}</option>);
+              } return (<option key={ i }>{coins.code}</option>);
             })}
           </select>
         </label>
