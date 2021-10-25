b3f6483e829258b1b36447e62cdb92ac516cb41c
unamednada <dias.gustavo.dias@gmail.com>
2021-10-25 18:07:51 -0300

    Create value input field in wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Input from '../components/Input';
 
 class Wallet extends React.Component {
   render() {
@@ -7,6 +8,19 @@ class Wallet extends React.Component {
       <div>
         <h1>TrybeWallet</h1>
         <Header />
+        <form
+          onSubmit={ (e) => {
+            e.preventDefault();
+          } }
+        >
+          <Input
+            htmlFor="value"
+            text="Valor"
+            value={ 0 }
+            handleChange={ () => null }
+            type="number"
+          />
+        </form>
       </div>
     );
   }
