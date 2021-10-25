99aa90ab0f40f86f86b17c526113c5f576066cf6
julianoprogrammierer <julianoprogrammierer@gmail.com>
2021-10-25 09:36:25 -0300

    Req 2 ao 5 'verificação dos campos, rota carteira e estado redux'

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const valor = 0;
+    const coin = 'BRL:';
+    const { email } = this.props;
+    return (
+      <div>
+        <h1 data-testid="email-field">
+          Seu Email:
+          { email }
+        </h1>
+        <h1 data-testid="total-field">
+          Despesa:
+          { valor }
+        </h1>
+        <h1 data-testid="header-currency-field">
+          Cambio:
+          { coin }
+        </h1>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+export default connect(mapStateToProps)(Header);
