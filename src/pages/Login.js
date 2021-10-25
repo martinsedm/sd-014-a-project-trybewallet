89b35550d062e87c590fd8de1df81c39506c0842
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-25 12:23:03 -0300

    Corrige requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { email, password } = this.state;
+    // https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
+    const verifyEmail = /\S+@\S+\.\S+/;
+
     return (
       <div>
         <input
@@ -9,14 +28,21 @@ class Login extends React.Component {
           name="email"
           pĺaceholder="E-mail"
           data-testid="email-input"
+          onChange={ this.handleChange }
         />
         <input
           type="password"
           name="password"
           pĺaceholder="Senha"
           data-testid="password-input"
+          onChange={ this.handleChange }
         />
-        <button type="submit">Entrar</button>
+        <button
+          type="submit"
+          onClick={ this.handleChange }
+          disabled={ !verifyEmail.test(email) || password.length < 6 }>
+            Entrar
+        </button>
       </div>
     );
   }
