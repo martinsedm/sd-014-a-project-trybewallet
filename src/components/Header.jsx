94bdb8d47d2f34c4395d73be6fc738c258d5b68d
unamednada <dias.gustavo.dias@gmail.com>
2021-10-25 18:02:11 -0300

    Create total and currency field in header. Requirement 05 complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,13 @@ import { connect } from 'react-redux';
 class Header extends React.Component {
   render() {
     const { user: { email } } = this.props;
+    const INITIAL_TOTAL = 0;
+    const CURRENCY = 'BRL';
     return (
       <div>
         <p data-testid="email-field">{ `Email: ${email}` }</p>
+        <p data-testid="total-field">{ `Despesa total: ${INITIAL_TOTAL}` }</p>
+        <p data-testid="header-currency-field">{ CURRENCY }</p>
       </div>
     );
   }
