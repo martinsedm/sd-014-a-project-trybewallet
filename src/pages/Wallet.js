597aeb8defde2576450a41b68b5c5341159a7607
dandara-dias <dandaradias.contato@gmail.com>
2021-10-25 01:42:05 -0300

    Requisito 6, parece que passando

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Expenses from '../components/Expenses';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Expenses />
+      </>
     );
   }
 }
