d18797badc47b3c7b43520d0f2792e546a1fd6b5
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-25 11:57:44 -0300

    Requisito 2 ok

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,23 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SAVE_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SAVE_EMAIL:
+    return {
+      ...state,
+      email: action.payload.email,
+      password: action.payload.password,
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
