385e9e4578b05267dcc8c0d23c1e67c1c1d837a1
Lucas Freitas <lucasr.freitas@icloud.com>
2021-10-25 18:35:19 -0300

    Resolve requisito 6

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,45 +1,74 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.paymentMethods = this.paymentMethods.bind(this);
+    this.categoryTypes = this.categoryTypes.bind(this);
+  }
+
+  paymentMethods() {
+    const arrayMethods = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+    return arrayMethods.map((method, index) => (
+      <option value={ method } key={ index }>{ method }</option>
+    ));
+  }
+
+  categoryTypes() {
+    const arrayCategories = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+    return arrayCategories.map((category, index) => (
+      <option value={ category } key={ index }>{ category }</option>
+    ));
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <label htmlFor="valor">
-            Valor:
-            <input
-              type="number"
-              name="valor"
-            />
-          </label>
-          <label htmlFor="descrição">
-            Descrição:
-            <input
-              type="text"
-              name="descrição"
-            />
-          </label>
-          <label htmlFor="moeda">
-            Moeda:
-            <select
-              name="moeda"
-            >
-              function a fazer
-            </select>
-          </label>
-          <label htmlFor="tag">
-            Método de Pagamento:
-            <select
-              name="tag"
-            >
-              function a fazer 2
-            </select>
-          </label>
-        </form>
-
-      </div>
+      <form>
+        <label htmlFor="valor">
+          Valor:
+          <input
+            type="number"
+            id="valor"
+          />
+        </label>
+        <label htmlFor="descrição">
+          Descrição:
+          <input
+            type="text"
+            id="descrição"
+          />
+        </label>
+        <label htmlFor="moeda">
+          Moeda:
+          <select
+            id="moeda"
+          >
+            requisita API
+          </select>
+        </label>
+        <label htmlFor="payment-methods">
+          Método de Pagamento:
+          <select
+            id="payment-methods"
+            name="payment-methods"
+          >
+            { this.paymentMethods() }
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag:
+          <select
+            id="tag"
+            name="tag"
+          >
+            { this.categoryTypes() }
+          </select>
+        </label>
+      </form>
     );
   }
 }
 
-export default Form;
+export default connect()(Form);
