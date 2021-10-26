0bd5d7cdb11c3949fffcc64aaf854f3786c6b204
Yan1451 <yan.brasileiro@gmail.com>
2021-10-26 11:27:37 -0300

    Reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const LOGIN = 'LOGIN';
+
+const USER_INITIALSTATE = {
+  user: {
+    email: '',
+    senha: '',
+  },
+};
+
+const user = (state = USER_INITIALSTATE, action) => {
+  switch (action.type) {
+  case LOGIN:
+    return { email: action.email, senha: action.senha };
+  default:
+    return state;
+  }
+};
+
+export { user, LOGIN };
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,23 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const RESPONSE_API = 'RESPONSE_API';
+const ERROR_API = 'ERROR_API';
+const ADICIONAR_GASTOS = 'ADICIONAR_GASTOS';
+
+const INITIALSTATE_API = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIALSTATE_API, action) => {
+  switch (action.type) {
+  case RESPONSE_API:
+    return { ...state, currencies: action.payload };
+  case ADICIONAR_GASTOS:
+    return { ...state,
+      expenses: [...state.expenses, { id: state.expenses.length, ...action.payload }] };
+  default:
+    return state;
+  }
+};
+
+export { RESPONSE_API, ERROR_API, wallet, ADICIONAR_GASTOS };
