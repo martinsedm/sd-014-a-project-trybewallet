b4771fc3df79f448697cce82d62e17768f32d634
Yan1451 <yan.brasileiro@gmail.com>
2021-10-26 11:24:14 -0300

    Requisito 1,2,3 , Pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const { email } = this.state;
+    const { history, loginDispatch } = this.props;
+    loginDispatch(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, senha } = this.state;
+    const EMAIL_REGEX = /.+@.+\..+/;
+    const SENHA_LENGTH = 6;
+    return (
+      <form>
+        <input
+          type="text"
+          name="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="text"
+          name="senha"
+          data-testid="password-input"
+          value={ senha }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          disabled={ !EMAIL_REGEX.test(email) || senha.length < SENHA_LENGTH }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  loginDispatch: (email) => dispatch(login(email)),
+});
+
+Login.propTypes = {
+  loginDispatch: PropTypes.func.isRequired,
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
