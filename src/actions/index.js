64caf9137302323f92d916243ef759c69b6ff348
Yan1451 <yan.brasileiro@gmail.com>
2021-10-26 11:27:46 -0300

    Actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,37 @@
 // Coloque aqui suas actions
+import { LOGIN } from '../reducers/user';
+import { RESPONSE_API, ERROR_API, ADICIONAR_GASTOS } from '../reducers/wallet';
+
+const login = (email, senha) => ({
+  type: LOGIN,
+  email,
+  senha,
+});
+
+const adicionarGastos = (payload) => ({
+  type: ADICIONAR_GASTOS,
+  payload,
+});
+
+const fetchApi = (payload) => ({
+  type: RESPONSE_API,
+  payload,
+});
+
+const errorApi = (payload) => ({
+  type: ERROR_API,
+  payload,
+});
+
+const api = () => async (dispatch) => {
+  try {
+    const resposta = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const respostaApi = await resposta.json();
+    delete respostaApi.USDT;
+    dispatch(fetchApi(respostaApi));
+  } catch (ERROR) {
+    return dispatch(errorApi(ERROR));
+  }
+};
+
+export { api, login, adicionarGastos };
