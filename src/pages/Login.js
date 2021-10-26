63b70699709700da4092bd821c7b0e68e04af6ef
Guilherme Roos <guilherme.roosr@gmail.com>
2021-10-26 01:06:14 -0300

    Criado página login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            Login:
+            <input
+              id="email"
+              type="email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              id="password"
+              type="password"
+              data-testid="password-input"
+            />
+          </label>
+          <button
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
