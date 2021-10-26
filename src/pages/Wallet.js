c1c6fe31311e6bde2d99c287d4157f15f44cb0fb
juan victor <juanvictor70@gmail.com>
2021-10-26 08:22:36 -0400

    trabalhando na  wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import Header from '../components/Header';
+import AddExpense from '../components/AddExpense';
+import Table from '../components/Table';
+import EditExpense from '../components/EditExpense';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      edit: false,
+      id: '',
+    };
+
+    this.handleEdit = this.handleEdit.bind(this);
+    this.finishEdit = this.finishEdit.bind(this);
+  }
+
+  handleEdit(itemId) {
+    this.setState({ edit: true, id: itemId });
+  }
+
+  finishEdit() {
+    this.setState({ edit: false, id: '' });
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { edit, id } = this.state;
+
+    return (
+      <>
+        <Header />
+        { !edit && <AddExpense />}
+        { edit && <EditExpense id={ id } onClick={ this.finishEdit } />}
+        <Table onClick={ this.handleEdit } />
+      </>
+    );
   }
 }
 
