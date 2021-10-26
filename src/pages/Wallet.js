c8b0bbe51dcc3904f56f3152e87f07b77010aff1
MartinSilva <martinempreendedor@gmail.com>
2021-10-25 22:18:02 -0300

    item 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <header data-testid="email-field">
+          Usuário não logado
+          <section data-testid="total-field">
+            0
+          </section>
+          <section data-testid="header-currency-field">
+            BRL
+          </section>
+        </header>
+      </div>);
   }
 }
 
