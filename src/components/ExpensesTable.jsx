6697c0442b8b2e7cb203aa18a713e445a11b6204
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-10-26 16:32:46 -0300

    Aplicaçao ok

--- src/components/ExpensesTable.jsx
+++ src/components/ExpensesTable.jsx
@@ -6,51 +6,53 @@ import { deleteExpense } from '../actions';
 class ExpensesTable extends React.Component {
   render() {
     const { expenses, deleteItem } = this.props;
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            expenses.map((expense) => (
-              <tr key={ expense.id }>
-                <td>{ expense.description }</td>
-                <td>{ expense.tag }</td>
-                <td>{ expense.method }</td>
-                <td>{ expense.value }</td>
-                <td>{ expense.exchangeRates[expense.currency].name.split('/')[0] }</td>
-                <td>
-                  { parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2) }
-                </td>
-                <td>
-                  { parseFloat(expense.value
-                  * parseFloat(expense.exchangeRates[expense.currency].ask)).toFixed(2)}
-                </td>
-                <td>Real</td>
-                <button
-                  type="button"
-                  data-testid="delete-btn"
-                  onClick={ () => deleteItem(expense.id) }
-                >
-                  Excluir
-                </button>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
-    );
+    if (expenses !== undefined) {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              expenses.map((expense) => (
+                <tr key={ expense.id }>
+                  <td>{ expense.description }</td>
+                  <td>{ expense.tag }</td>
+                  <td>{ expense.method }</td>
+                  <td>{ expense.value }</td>
+                  <td>{ expense.exchangeRates[expense.currency].name.split('/')[0] }</td>
+                  <td>
+                    { parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2) }
+                  </td>
+                  <td>
+                    { parseFloat(expense.value
+                    * parseFloat(expense.exchangeRates[expense.currency].ask)).toFixed(2)}
+                  </td>
+                  <td>Real</td>
+                  <button
+                    type="button"
+                    data-testid="delete-btn"
+                    onClick={ () => deleteItem(expense.id) }
+                  >
+                    Excluir
+                  </button>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      );
+    }
   }
 }
 
