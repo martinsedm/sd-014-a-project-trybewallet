61e0d3622b28405a74d0a9a479a66568940bb375
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-26 00:35:30 -0300

    08 - Display total expenses in BRL

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-function Header({ user }) {
+function Header({ expenses, user }) {
+  const total = expenses.reduce((acc, curr) => {
+    const value = Number(curr.value);
+    const exchange = Number(curr.exchangeRates[curr.currency].ask);
+    return acc + value * exchange;
+  }, 0);
+  // Calculate total in BRL
+
   return (
     <header>
       <h1>Cabeçalho</h1>
       <div>
         <p data-testid="email-field">{user}</p>
         {/* change to span tag when styling */}
-        <span data-testid="total-field">0</span>
+        <span data-testid="total-field">{total}</span>
         <span data-testid="header-currency-field">BRL</span>
       </div>
     </header>
@@ -17,10 +24,12 @@ function Header({ user }) {
 }
 
 const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
   user: state.user.email,
 });
 
 Header.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
   user: PropTypes.string.isRequired,
 };
 
