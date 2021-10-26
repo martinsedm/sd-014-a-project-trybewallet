8b1c5071354bf1cb01d78ae87a8ffe24ea929b0b
juan victor <juanvictor70@gmail.com>
2021-10-26 08:09:58 -0400

    trabalhando na tela de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,53 @@
 // Coloque aqui suas actions
+export const USER_LOGIN = 'USER_LOGIN';
+export const UPDATE_CURRENCY = 'UPDATE_CURRENCY';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
+export const DELETE_EXPENSE = 'DELETE_EXPENSE';
+export const SET_LOADING = 'SET_LOADING';
+
+export const userLogin = (email) => ({
+  type: USER_LOGIN,
+  payload: email,
+});
+
+export const loading = () => ({
+  type: SET_LOADING,
+});
+
+export const updateCurrency = (currency) => ({
+  type: UPDATE_CURRENCY,
+  payload: currency,
+});
+
+export const deleteExpense = (newCurrencies) => ({
+  type: DELETE_EXPENSE,
+  payload: newCurrencies,
+});
+
+export const updateExpense = (expense) => ({
+  type: ADD_EXPENSE,
+  payload: expense,
+});
+
+export const editExpense = (expense) => ({
+  type: EDIT_EXPENSE,
+  payload: expense,
+});
+
+export const fetchExpense = (state) => (dispatch) => (
+  fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((response) => dispatch(updateExpense({ ...state, exchangeRates: response })))
+);
+
+export const fetchCurrency = () => (dispatch) => {
+  dispatch(loading());
+  return fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((response) => {
+      const allCurrencies = Object.keys(response);
+      const USDTdeleted = allCurrencies.filter((currency) => currency !== 'USDT');
+      dispatch(updateCurrency(USDTdeleted));
+    });
+};
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,94 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { userLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      blockSubmit: true, // Bloqueio da submissão para o botão.
+      redirect: false,
+    };
+
+    this.validateInputs = this.validateInputs.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  validateInputs() {
+    const { email, password } = this.state;
+    const validEmail = /\S+@\S+\.\S+/; // Validação do e-mail com Regex.
+    const validPassword = 6;
+    if (validEmail.test(email) && password.length >= validPassword) { // .test para saber se é correspondente e retorna um bool.
+      this.setState({ blockSubmit: false });
+    } else {
+      this.setState({ blockSubmit: true });
+    }
+  }
+
+  // Controlled Components
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => this.validateInputs());
+  }
+
+  handleClick() {
+    const { auth } = this.props;
+    const { email } = this.state;
+    auth(email);
+    this.setState({ redirect: true });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, blockSubmit, redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to="/carteira" />;
+    }
+
+    return (
+      <main>
+        <form>
+          <input
+            name="email"
+            type="text"
+            placeholder="EMAIL"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="SENHA"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            disabled={ blockSubmit }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
+      </main>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  auth: (email) => dispatch(userLogin(email)),
+});
+
+Login.propTypes = {
+  auth: propTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
