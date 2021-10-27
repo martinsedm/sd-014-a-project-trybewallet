4eddf592e09a555882522d013cab637ef6304607
Marcio <marciofonseca.ufrj@gmail.com>
2021-10-26 22:23:57 -0300

    Inciando projeto

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,11 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+function walletReducer(state, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
