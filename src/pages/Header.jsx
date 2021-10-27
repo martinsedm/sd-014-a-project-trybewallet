488bac2e3c3677ea2ec15cf8cbddf9f7bf64831b
Lucas G Silveira <lucgs88@gmail.com>
2021-10-27 18:42:56 -0300

    item 6

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -13,8 +13,7 @@ class Header extends Component {
 
   render() {
     const { login } = this.props;
-    console.log(login);
-
+    
     return (
       <section>
         <div data-testid="email-field">
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -2,11 +2,58 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from './Header';
 
+const tag = ['Alimentação', 'Transporte', 'Lazer', 'Trabalho', 'Saúde', 'Outros'];
+const payment = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+const currency = ['BRL', 'USD', 'EUR'];
+
 class Wallet extends Component {
+  constructor(props) {
+    super(props);
+    this.createInput = this.createInput.bind(this);
+  }
+
+  createInput(id, type, value) {
+    return (
+      <>
+        <form>
+          <label htmlFor={ id }>
+            { value }
+            <input type={ type } name={ id } id={ id } />
+          </label>
+        </form>
+        <p />
+      </>
+    );
+  }
+
+  createSelect(id, obj, value) {
+    return (
+      <>
+        <form>
+          <label htmlFor={ id }>
+            { value }
+            <select name={ id } id={ id }>
+              { obj.map((element, i) => (
+                <option key={ i } value={ element }>{ element }</option>
+              ))}
+            </select>
+          </label>
+        </form>
+        <p />
+      </>
+    );
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <p />
+        { this.createInput('value', 'text', 'Valor:') }
+        { this.createInput('description', 'text', 'Descrição:') }
+        { this.createSelect('currency', currency, 'Moeda:') }
+        { this.createSelect('payment', payment, 'Método de pagamento:') }
+        { this.createSelect('tag', tag, 'Tag:') }
       </div>
     );
   }
