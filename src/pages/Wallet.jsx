4a33d5e076b5d31a5e2470fef63387435bfc099b
Lucas G Silveira <lucgs88@gmail.com>
2021-10-28 15:29:33 -0300

    item7

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -4,12 +4,25 @@ import Header from './Header';
 
 const tag = ['Alimentação', 'Transporte', 'Lazer', 'Trabalho', 'Saúde', 'Outros'];
 const payment = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
-const currency = ['BRL', 'USD', 'EUR'];
 
 class Wallet extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currency: [],
+    };
+
     this.createInput = this.createInput.bind(this);
+    this.createSelect = this.createSelect.bind(this);
+    this.fetchCurrency = this.fetchCurrency.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCurrency().then((response) => {
+      this.setState({
+        currency: response,
+      });
+    });
   }
 
   createInput(id, type, value) {
@@ -26,14 +39,23 @@ class Wallet extends Component {
     );
   }
 
-  createSelect(id, obj, value) {
+  removeCurrency() {
+    const { currency } = this.state;
+    const newCurrency = Object.keys(currency).filter((element) => element !== 'USDT');
+    this.setState({
+      currency: newCurrency,
+    });
+    console.log(newCurrency);
+  }
+
+  createSelect(id, obj = {}, value) {
     return (
       <>
         <form>
           <label htmlFor={ id }>
             { value }
             <select name={ id } id={ id }>
-              { obj.map((element, i) => (
+              { Object.values(obj).map((element, i) => (
                 <option key={ i } value={ element }>{ element }</option>
               ))}
             </select>
@@ -44,14 +66,23 @@ class Wallet extends Component {
     );
   }
 
+  async fetchCurrency() {
+    const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const data = await response.json();
+    return data;
+  }
+
   render() {
+    const { currency } = this.state;
+    const newCurrency = Object.keys(currency).filter((element) => element !== 'USDT');
+
     return (
       <div>
         <Header />
         <p />
         { this.createInput('value', 'text', 'Valor:') }
         { this.createInput('description', 'text', 'Descrição:') }
-        { this.createSelect('currency', currency, 'Moeda:') }
+        { this.createSelect('currency', newCurrency, 'Moeda:') }
         { this.createSelect('payment', payment, 'Método de pagamento:') }
         { this.createSelect('tag', tag, 'Tag:') }
       </div>
