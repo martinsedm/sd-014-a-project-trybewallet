a59c9b94e7f04c9d884966836830b196d600f7a1
Veruska de Oliveira <veruska.ideasdigital@gmail.com>
2021-11-01 23:21:23 +0000

    Requisito 09

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,31 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Form from '../components/Form';
+import Expenses from '../components/Expenses';
 
 class Wallet extends React.Component {
   totalValueExpenses() {
     const { totalArray } = this.props;
-
     const total = totalArray.reduce((acc, curr) => {
       const currencyExpense = curr.currency;
 
       const objExchangeRates = curr.exchangeRates[currencyExpense];
-      const askExchangeRates = objExchangeRates.ask;
-      const number = parseInt(curr.value, 10);
+      const askExchangeRates = parseFloat(objExchangeRates.ask);
+      const number = parseFloat(curr.value);
 
       const valueExchange = number * askExchangeRates;
 
       acc += valueExchange;
-
       return acc;
     }, 0);
-
     return total.toFixed(2);
   }
-
+  
   render() {
     const { email } = this.props;
-
     return (
       <>
         <header>
@@ -35,6 +32,7 @@ class Wallet extends React.Component {
           <div data-testid="header-currency-field">BRL</div>
         </header>
         <Form />
+        <Expenses />
       </>
     );
   }
