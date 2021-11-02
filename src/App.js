44e018aabb6ed29076efd6df6699f16d2b5238aa
jhonathancs <jhonathan_jcs@hotmail.com>
2021-11-02 13:20:50 -0500

    item03-LintCorrigido

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 import SwitcherRoute from './component/SwitcherRoute';
-import store from './store';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Provider store={ store }>
-          <SwitcherRoute />
-        </Provider>
+        <SwitcherRoute />
       </div>
     );
   }
