a9d1a7a2c4e3f25bce8399de0dbedeb4a730335e
Isaddora Bayma <isadora.bayma@gmail.com>
2021-11-03 01:37:54 -0300

    erro store export lint

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './store';
+import ConnectedStore from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={ store }>
+  <Provider store={ ConnectedStore }>
     <BrowserRouter>
       <App />
     </BrowserRouter>
