beebe9a2ac5ba6c418f02dc6199ea4032a692bd5
Victor Veloso <victorveloso13@gmail.com>
2021-11-04 09:51:05 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="Insira o seu email"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Insira a sua senha"
+        />
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
