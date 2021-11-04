d751dca521f6db2bd925b06b221f2a162970d350
Victor Veloso <victorveloso13@gmail.com>
2021-11-04 20:07:43 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,55 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validateAcess = this.validateAcess.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  // Realizei essa função de validação de email consultando https://ui.dev/validate-email-address-javascript/
+  validateAcess(email, password) {
+    const minCharacter = 6;
+    const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const passwordValid = password.length >= minCharacter;
+    return emailValid && passwordValid;
+  }
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <form>
         <input
           type="email"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
           data-testid="email-input"
           placeholder="Insira o seu email"
         />
         <input
           type="password"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
           data-testid="password-input"
           placeholder="Insira a sua senha"
         />
-        <button type="submit">Entrar</button>
+        <button
+          type="submit"
+          disabled={ !this.validateAcess(email, password) }
+        >
+          Entrar
+        </button>
       </form>
     );
   }
