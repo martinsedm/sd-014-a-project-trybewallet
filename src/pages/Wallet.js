5fb618097275e94fe673ffe0b9b63f4f9d4917d3
jhonathancs <jhonathan_jcs@hotmail.com>
2021-11-03 19:43:46 -0500

    LintResolvido

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Forms from '../component/Forms';
+import Header from '../component/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        TrybeWallet
+        <Header />
+        <Forms />
+      </div>
+    );
   }
 }
 
