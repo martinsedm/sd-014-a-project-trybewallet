03279f43318487773f8533a55358af7329dd9029
Caio César <caiovg32@gmail.com>
2021-11-05 14:35:33 -0300

    req 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+const newUser = (state) => ({
+  type: 'ADD_NEW_USER',
+  state,
+});
+
+export default newUser;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,7 @@
-const INITIAL_STATE = {
-  user: {
-    email: '',
-  },
-  wallet: {
-    currencies: [],
-    expenses: [],
-  },
-};
+import { combineReducers } from 'redux';
+import user from './user';
+import wallet from './wallet';
 
-function reducer(state = INITIAL_STATE, action) {
-  switch (action.type) {
-  case 'ADD_NEW_EXPENSE':
-    return (action.state);
-  default:
-    return state;
-  }
-}
+const rootreducer = combineReducers({ user, wallet });
 
-export default reducer;
+export default rootreducer;
