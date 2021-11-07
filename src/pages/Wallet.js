3f32cb56b286d515453cd93390c36a8ec292b05c
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-11-07 10:17:42 -0300

    creates wallet page and not found and reducers

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import Header from '../components/Header';
+import AddPayment from '../components/AddPayment';
+import EditPayment from '../components/EditPayment';
+import PaymenTable from '../components/PaymentTable';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      id: '',
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleEdition = this.handleEdition.bind(this);
+  }
+
+  onSubmit() {
+    this.setState({
+      editing: false,
+      id: '',
+    });
+  }
+
+  handleEdition(id) {
+    this.setState({
+      editing: true,
+      id,
+    });
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { editing, id } = this.state;
+    return (
+      <>
+        <Header />
+        {editing ? <EditPayment onSubmit={ this.onSubmit } id={ id } /> : <AddPayment />}
+        <PaymenTable handleEdition={ this.handleEdition } />
+      </>
+    );
   }
 }
-
 export default Wallet;
