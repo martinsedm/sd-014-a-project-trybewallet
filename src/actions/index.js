9b264e0117402886de21d77ffcc8e44d1b00075e
Lua <luabaronto@hotmail.com>
2021-11-08 15:01:30 -0300

    index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,26 @@
 // Coloque aqui suas actions
+export const USER_DATA = 'USER_DATA';
+export const SUCCESS_FETCH = 'SUCCESS_FETCH';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const SEND_EXPENSES = 'SEND_EXPENSES';
+export const DEL_EXPENSE = 'DEL_EXPENSE';
+
+export const userData = (email) => ({
+  type: 'USER_DATA',
+  email,
+});
+
+export const successFetch = (coin) => ({
+  type: 'SUCCESS_FETCH',
+  coin,
+});
+
+export const addExpense = (expense) => ({
+  type: 'ADD_EXPENSE',
+  expense,
+});
+
+export const deleteExpense = (payload) => ({
+  type: 'DEL_EXPENSE',
+  payload,
+});
