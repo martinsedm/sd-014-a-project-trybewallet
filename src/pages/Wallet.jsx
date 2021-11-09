5ba440ecdc64a934101fed83bf9f5fb83d405302
PeDD1 <pedrohr0909@gmail.com>
2021-11-09 02:14:18 -0300

    Added Form to Wallet

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -6,6 +6,40 @@ class Wallet extends React.Component {
     return (
       <div>
         <Header />
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input id="value" type="text" name="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input id="description" type="text" name="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda:
+            <select id="currency" type="select" name="currency">
+              <option>moeda</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento:
+            <select id="payment" name="payment">
+              <option>Dinheiro</option>
+              <option>Cartão de Crédito</option>
+              <option>Cartão de Débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag:
+            <select id="tag" name="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
