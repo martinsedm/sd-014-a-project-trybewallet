d6c5d67a855abfe9a805eb6ccfa4e191734601e8
PeDD1 <pedrohr0909@gmail.com>
2021-11-09 01:08:45 -0300

    Added route and Header to Wallet

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <div>
+        <p data-testid="email-field">
+          {email}
+        </p>
+        <p data-testid="total-field"> 0</p>
+        <p data-testid="header-currency-field"> BRL</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
